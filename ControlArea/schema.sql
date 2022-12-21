851d4c3ee098598b3fea78fac7685b6494a6147d
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:56:20 +0100

    test of SQL export

---
+++ ControlArea/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ControlArea of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ControlArea_type AS ENUM ('ControlArea');
+CREATE TABLE ControlArea (ControlAreaGeneratingUnit text, EnergyArea text, TieFlow text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, netInterchange text, owner json, pTolerance text, seeAlso json, source text, type ControlArea_type);
