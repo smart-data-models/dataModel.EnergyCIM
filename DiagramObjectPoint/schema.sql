695a28f696940b192c00edf3f9a956294531ee78
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:57:16 +0100

    test of SQL export

---
+++ DiagramObjectPoint/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model DiagramObjectPoint of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE DiagramObjectPoint_type AS ENUM ('DiagramObjectPoint');
+CREATE TABLE DiagramObjectPoint (DiagramObject text, DiagramObjectGluePoint text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, sequenceNumber text, source text, type DiagramObjectPoint_type, xPosition text, yPosition text, zPosition text);
