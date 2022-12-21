8f32ab0aedec25d5f23e40c994ffa9548632809b
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:55:29 +0100

    test of SQL export

---
+++ AngleRadians/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model AngleRadians of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE AngleRadians_type AS ENUM ('AngleRadians');
+CREATE TABLE AngleRadians (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type AngleRadians_type, unit text, value text);
