4548806ffbd4e5cf75a8f2ab2a996173945e3eeb
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:03:24 +0100

    test of SQL export

---
+++ OverexcLimX2/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model OverexcLimX2 of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE OverexcLimX2_type AS ENUM ('OverexcLimX2');
+CREATE TABLE OverexcLimX2 (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, efd1 text, efd2 text, efd3 text, efddes text, efdrated text, id text, kmx text, location json, m text, name text, owner json, seeAlso json, source text, t1 text, t2 text, t3 text, type OverexcLimX2_type, vlow text);
