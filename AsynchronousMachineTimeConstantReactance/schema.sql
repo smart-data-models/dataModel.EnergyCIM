06fe6665a4ca7e8674251cde96a979af1c6938c4
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:55:44 +0100

    test of SQL export

---
+++ AsynchronousMachineTimeConstantReactance/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model AsynchronousMachineTimeConstantReactance of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE AsynchronousMachineTimeConstantReactance_type AS ENUM ('AsynchronousMachineTimeConstantReactance');
+CREATE TABLE AsynchronousMachineTimeConstantReactance (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, tpo text, tppo text, type AsynchronousMachineTimeConstantReactance_type, xp text, xpp text, xs text);
