9ac4ef6b181aceb15524aa6cdd5cf98a15526952
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:55:58 +0100

    test of SQL export

---
+++ Capacitance/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Capacitance of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Capacitance_type AS ENUM ('Capacitance');
+CREATE TABLE Capacitance (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type Capacitance_type, unit text, value text);
