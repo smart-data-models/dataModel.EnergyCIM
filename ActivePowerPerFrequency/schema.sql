0d50aaf57a9dcadc2ecb72fc8a14f3cefa210f34
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:55:14 +0100

    test of SQL export

---
+++ ActivePowerPerFrequency/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ActivePowerPerFrequency of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ActivePowerPerFrequency_type AS ENUM ('ActivePowerPerFrequency');
+CREATE TABLE ActivePowerPerFrequency (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, denominatorMultiplier text, denominatorUnit text, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type ActivePowerPerFrequency_type, unit text, value text);
