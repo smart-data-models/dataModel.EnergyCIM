1fba38ca7224306c1808dad41d14de7604e24c96
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:06:23 +0100

    test of SQL export

---
+++ SvPowerFlow/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model SvPowerFlow of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE SvPowerFlow_type AS ENUM ('SvPowerFlow');
+CREATE TABLE SvPowerFlow (Terminal text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, p text, q text, seeAlso json, source text, type SvPowerFlow_type);
