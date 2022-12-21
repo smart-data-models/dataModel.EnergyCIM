6c251e0df8dcd74db9c379b06e021add8ad07fe6
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:57:13 +0100

    test of SQL export

---
+++ DiagramObjectGluePoint/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model DiagramObjectGluePoint of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE DiagramObjectGluePoint_type AS ENUM ('DiagramObjectGluePoint');
+CREATE TABLE DiagramObjectGluePoint (DiagramObjectPoints text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type DiagramObjectGluePoint_type);
