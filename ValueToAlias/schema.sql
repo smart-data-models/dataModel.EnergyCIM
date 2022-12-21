9c3d6b7a7362f870400e5875c5808f49c713c985
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:07:50 +0100

    test of SQL export

---
+++ ValueToAlias/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ValueToAlias of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ValueToAlias_type AS ENUM ('ValueToAlias');
+CREATE TABLE ValueToAlias (ValueAliasSet text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type ValueToAlias_type, value text);
