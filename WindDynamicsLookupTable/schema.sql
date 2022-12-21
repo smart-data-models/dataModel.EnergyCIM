112151763824ea0d82f36910156faf3479901e64
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:08:44 +0100

    test of SQL export

---
+++ WindDynamicsLookupTable/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model WindDynamicsLookupTable of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE WindDynamicsLookupTable_type AS ENUM ('WindDynamicsLookupTable');
+CREATE TABLE WindDynamicsLookupTable (WindContCurrLimIEC text, WindContPType3IEC text, WindContRotorRIEC text, WindPlantFreqPcontrolIEC text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, input text, location json, lookupTableFunctionType text, name text, output text, owner json, seeAlso json, sequence text, source text, type WindDynamicsLookupTable_type);
