743fa2d12a64a4a1dc15565fa6a8cdcd496489b8
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:02:43 +0100

    test of SQL export

---
+++ MeasurementValueSource/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model MeasurementValueSource of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE MeasurementValueSource_type AS ENUM ('MeasurementValueSource');
+CREATE TABLE MeasurementValueSource (MeasurementValues text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type MeasurementValueSource_type);
