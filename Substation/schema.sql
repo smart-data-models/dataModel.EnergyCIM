99eeade487a86c58dac8dd84dfdc8528b22f1e76
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:06:17 +0100

    test of SQL export

---
+++ Substation/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Substation of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Substation_type AS ENUM ('Substation');
+CREATE TABLE Substation (DCConverterUnit text, Region text, VoltageLevels text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type Substation_type);
