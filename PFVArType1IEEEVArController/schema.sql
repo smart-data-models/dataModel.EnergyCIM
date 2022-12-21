a36f008625ace7d14c5798cb952eeaa349b0204b
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:03:44 +0100

    test of SQL export

---
+++ PFVArType1IEEEVArController/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model PFVArType1IEEEVArController of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE PFVArType1IEEEVArController_type AS ENUM ('PFVArType1IEEEVArController');
+CREATE TABLE PFVArType1IEEEVArController (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, tvarc text, type PFVArType1IEEEVArController_type, vvar text, vvarcbw text, vvarref text, vvtmax text, vvtmin text);
