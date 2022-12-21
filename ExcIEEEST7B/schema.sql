48445e2068f972b2e6b9c3db1e08f5f5476dfdf9
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:59:43 +0100

    test of SQL export

---
+++ ExcIEEEST7B/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ExcIEEEST7B of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ExcIEEEST7B_type AS ENUM ('ExcIEEEST7B');
+CREATE TABLE ExcIEEEST7B (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, kh text, kia text, kl text, kpa text, location json, name text, oelin text, owner json, seeAlso json, source text, tb text, tc text, tf text, tg text, tia text, type ExcIEEEST7B_type, uelin text, vmax text, vmin text, vrmax text, vrmin text);
