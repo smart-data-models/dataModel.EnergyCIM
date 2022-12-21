cbe633a978a869592cef2782f36b2f486571eb07
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:56:30 +0100

    test of SQL export

---
+++ CurrentLimit/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model CurrentLimit of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE CurrentLimit_type AS ENUM ('CurrentLimit');
+CREATE TABLE CurrentLimit (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type CurrentLimit_type, value text);
