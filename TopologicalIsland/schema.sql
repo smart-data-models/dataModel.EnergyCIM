27a9b414b502baaa644b3211e47d3c906b715a5d
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:07:08 +0100

    test of SQL export

---
+++ TopologicalIsland/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model TopologicalIsland of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE TopologicalIsland_type AS ENUM ('TopologicalIsland');
+CREATE TABLE TopologicalIsland (AngleRefTopologicalNode text, TopologicalNodes text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type TopologicalIsland_type);
