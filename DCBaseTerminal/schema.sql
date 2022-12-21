7dd26e895eac99c3bb43c9dd4c75a681c1e4ad7b
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:56:37 +0100

    test of SQL export

---
+++ DCBaseTerminal/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model DCBaseTerminal of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE DCBaseTerminal_type AS ENUM ('DCBaseTerminal');
+CREATE TABLE DCBaseTerminal (DCNode text, DCTopologicalNode text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type DCBaseTerminal_type);
