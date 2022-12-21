2ba5c6ce6de2fc3e9673724fe834f96b2a975bac
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:04:06 +0100

    test of SQL export

---
+++ PhaseTapChangerTablePoint/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model PhaseTapChangerTablePoint of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE PhaseTapChangerTablePoint_type AS ENUM ('PhaseTapChangerTablePoint');
+CREATE TABLE PhaseTapChangerTablePoint (PhaseTapChangerTable text, address json, alternateName text, angle text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type PhaseTapChangerTablePoint_type);
