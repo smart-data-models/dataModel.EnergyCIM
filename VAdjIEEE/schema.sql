e5702e3702248e0a46897158085920f62b5e7a64
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:07:46 +0100

    test of SQL export

---
+++ VAdjIEEE/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model VAdjIEEE of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE VAdjIEEE_type AS ENUM ('VAdjIEEE');
+CREATE TABLE VAdjIEEE (address json, adjslew text, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, taoff text, taon text, type VAdjIEEE_type, vadjf text, vadjmax text, vadjmin text);
