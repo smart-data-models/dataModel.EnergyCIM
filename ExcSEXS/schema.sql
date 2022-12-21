05a4779725d6fd076fb073e46efe0cefae612067
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:59:58 +0100

    test of SQL export

---
+++ ExcSEXS/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ExcSEXS of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ExcSEXS_type AS ENUM ('ExcSEXS');
+CREATE TABLE ExcSEXS (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, efdmax text, efdmin text, emax text, emin text, id text, k text, kc text, location json, name text, owner json, seeAlso json, source text, tatb text, tb text, tc text, te text, type ExcSEXS_type);
