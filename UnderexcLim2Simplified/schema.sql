8f8457f62df89d4104ec10c7fec27c60a260e946
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:07:35 +0100

    test of SQL export

---
+++ UnderexcLim2Simplified/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model UnderexcLim2Simplified of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE UnderexcLim2Simplified_type AS ENUM ('UnderexcLim2Simplified');
+CREATE TABLE UnderexcLim2Simplified (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, kui text, location json, name text, owner json, p0 text, p1 text, q0 text, q1 text, seeAlso json, source text, type UnderexcLim2Simplified_type, vuimax text, vuimin text);
