64563a529ed4b27a6295bf287e8574fd744c204d
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:05:24 +0100

    test of SQL export

---
+++ RegularTimePoint/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model RegularTimePoint of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE RegularTimePoint_type AS ENUM ('RegularTimePoint');
+CREATE TABLE RegularTimePoint (IntervalSchedule text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, sequenceNumber text, source text, type RegularTimePoint_type, value1 text, value2 text);
