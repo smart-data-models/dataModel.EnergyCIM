7ef28b5c09e09664c3f52118ded3b188faeb3da8
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:56:12 +0100

    test of SQL export

---
+++ ConformLoadGroup/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ConformLoadGroup of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ConformLoadGroup_type AS ENUM ('ConformLoadGroup');
+CREATE TABLE ConformLoadGroup (ConformLoadSchedules text, EnergyConsumers text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type ConformLoadGroup_type);
