ae23159d8ae31b66871558780f1e7560df96d5ef
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:06:29 +0100

    test of SQL export

---
+++ SvTapStep/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model SvTapStep of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE SvTapStep_type AS ENUM ('SvTapStep');
+CREATE TABLE SvTapStep (TapChanger text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, position text, seeAlso json, source text, type SvTapStep_type);
