5157acb730379100987aface4cb6210f8da9f0d7
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:55:52 +0100

    test of SQL export

---
+++ Bay/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Bay of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Bay_type AS ENUM ('Bay');
+CREATE TABLE Bay (VoltageLevel text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type Bay_type);
