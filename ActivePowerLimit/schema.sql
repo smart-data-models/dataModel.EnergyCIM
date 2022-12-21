f12f949516eec469eb9bf75426aedd812ea2ed87
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:55:09 +0100

    test of SQL export

---
+++ ActivePowerLimit/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ActivePowerLimit of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ActivePowerLimit_type AS ENUM ('ActivePowerLimit');
+CREATE TABLE ActivePowerLimit (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type ActivePowerLimit_type, value text);
