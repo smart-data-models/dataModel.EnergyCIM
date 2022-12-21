df45aedc1601946eb36791499de12694ff189dd5
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:05:40 +0100

    test of SQL export

---
+++ ResistancePerLength/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ResistancePerLength of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ResistancePerLength_type AS ENUM ('ResistancePerLength');
+CREATE TABLE ResistancePerLength (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, denominatorMultiplier text, denominatorUnit text, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type ResistancePerLength_type, unit text, value text);
