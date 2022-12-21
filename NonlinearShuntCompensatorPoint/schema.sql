4b425191544b573deadeb5808b92f56fa6f1891f
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:03:05 +0100

    test of SQL export

---
+++ NonlinearShuntCompensatorPoint/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model NonlinearShuntCompensatorPoint of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE NonlinearShuntCompensatorPoint_type AS ENUM ('NonlinearShuntCompensatorPoint');
+CREATE TABLE NonlinearShuntCompensatorPoint (NonlinearShuntCompensator text, address json, alternateName text, areaServed text, b text, b0 text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, g text, g0 text, id text, location json, name text, owner json, sectionNumber text, seeAlso json, source text, type NonlinearShuntCompensatorPoint_type);
