156632f911be32f684b9bff09209fe8cf178a1a5
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:04:16 +0100

    test of SQL export

---
+++ PowerSystemStabilizerUserDefined/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model PowerSystemStabilizerUserDefined of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE PowerSystemStabilizerUserDefined_type AS ENUM ('PowerSystemStabilizerUserDefined');
+CREATE TABLE PowerSystemStabilizerUserDefined (ProprietaryParameterDynamics text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, proprietary text, seeAlso json, source text, type PowerSystemStabilizerUserDefined_type);
