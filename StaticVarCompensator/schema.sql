e4f9e62bfe8832032821f099a6a64e6ae70c3dc8
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:06:05 +0100

    test of SQL export

---
+++ StaticVarCompensator/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model StaticVarCompensator of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE StaticVarCompensator_type AS ENUM ('StaticVarCompensator');
+CREATE TABLE StaticVarCompensator (address json, alternateName text, areaServed text, capacitiveRating text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, inductiveRating text, location json, name text, owner json, q text, sVCControlMode text, seeAlso json, slope text, source text, type StaticVarCompensator_type, voltageSetPoint text);
