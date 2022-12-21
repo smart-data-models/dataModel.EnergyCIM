fadd8a7d084614d8957f0593a9ff6f70b498d230
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:05:22 +0100

    test of SQL export

---
+++ RegularIntervalSchedule/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model RegularIntervalSchedule of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE RegularIntervalSchedule_type AS ENUM ('RegularIntervalSchedule');
+CREATE TABLE RegularIntervalSchedule (TimePoints text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, endTime text, id text, location json, name text, owner json, seeAlso json, source text, timeStep text, type RegularIntervalSchedule_type);
