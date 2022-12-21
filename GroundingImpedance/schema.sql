bf90727fb58f2dcaf6e4c499cd08369056db2e1d
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:01:48 +0100

    test of SQL export

---
+++ GroundingImpedance/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model GroundingImpedance of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE GroundingImpedance_type AS ENUM ('GroundingImpedance');
+CREATE TABLE GroundingImpedance (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type GroundingImpedance_type, x text);
