3cf6a70b7e5106f4c4c93c4a358fc1f4a26a2392
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:05:31 +0100

    test of SQL export

---
+++ RegulationSchedule/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model RegulationSchedule of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE RegulationSchedule_type AS ENUM ('RegulationSchedule');
+CREATE TABLE RegulationSchedule (RegulatingControl text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type RegulationSchedule_type);
