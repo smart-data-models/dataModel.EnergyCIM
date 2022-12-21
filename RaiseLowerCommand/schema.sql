d4fc5221554d15180763a19261895cac04269ca6
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:05:07 +0100

    test of SQL export

---
+++ RaiseLowerCommand/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model RaiseLowerCommand of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE RaiseLowerCommand_type AS ENUM ('RaiseLowerCommand');
+CREATE TABLE RaiseLowerCommand (ValueAliasSet text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type RaiseLowerCommand_type);
