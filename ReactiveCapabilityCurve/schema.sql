67fc84be31d1ee733730d306feca9bf02add2061
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:05:18 +0100

    test of SQL export

---
+++ ReactiveCapabilityCurve/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ReactiveCapabilityCurve of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ReactiveCapabilityCurve_type AS ENUM ('ReactiveCapabilityCurve');
+CREATE TABLE ReactiveCapabilityCurve (EquivalentInjection text, InitiallyUsedBySynchronousMachines text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type ReactiveCapabilityCurve_type);
