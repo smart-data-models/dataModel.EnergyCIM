34628af0f5b321ce66726c19c3b99255b20a98d4
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:07:24 +0100

    test of SQL export

---
+++ TurbineLoadControllerDynamics/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model TurbineLoadControllerDynamics of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE TurbineLoadControllerDynamics_type AS ENUM ('TurbineLoadControllerDynamics');
+CREATE TABLE TurbineLoadControllerDynamics (TurbineGovernorDynamics text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type TurbineLoadControllerDynamics_type);
