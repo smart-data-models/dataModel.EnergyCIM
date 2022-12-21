e05d1e4b3ce5d1a4e00bb972fc17e565624fd9ca
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:58:08 +0100

    test of SQL export

---
+++ EquivalentNetwork/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model EquivalentNetwork of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE EquivalentNetwork_type AS ENUM ('EquivalentNetwork');
+CREATE TABLE EquivalentNetwork (EquivalentEquipments text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type EquivalentNetwork_type);
