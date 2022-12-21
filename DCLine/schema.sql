76f2de13e63e958339813056971577f017244a9f
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:56:49 +0100

    test of SQL export

---
+++ DCLine/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model DCLine of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE DCLine_type AS ENUM ('DCLine');
+CREATE TABLE DCLine (Region text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type DCLine_type);
