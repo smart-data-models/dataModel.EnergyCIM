0627a35ffe936f97b7c2ecacdde2d47efa1db3c4
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:57:37 +0100

    test of SQL export

---
+++ DynamicsFunctionBlock/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model DynamicsFunctionBlock of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE DynamicsFunctionBlock_type AS ENUM ('DynamicsFunctionBlock');
+CREATE TABLE DynamicsFunctionBlock (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, enabled text, id text, location json, name text, owner json, seeAlso json, source text, type DynamicsFunctionBlock_type);
