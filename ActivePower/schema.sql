ded9991366e3cc8bf411224f9a60831ae39e6957
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:55:07 +0100

    test of SQL export

---
+++ ActivePower/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ActivePower of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ActivePower_type AS ENUM ('ActivePower');
+CREATE TABLE ActivePower (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type ActivePower_type, unit text, value text);
