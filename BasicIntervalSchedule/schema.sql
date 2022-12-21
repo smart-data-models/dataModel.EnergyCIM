56b4b07c441c0c9f83559e6b844a1f35d1012de6
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:55:50 +0100

    test of SQL export

---
+++ BasicIntervalSchedule/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model BasicIntervalSchedule of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE BasicIntervalSchedule_type AS ENUM ('BasicIntervalSchedule');
+CREATE TABLE BasicIntervalSchedule (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, startTime text, type BasicIntervalSchedule_type, value1Unit text, value2Unit text);
