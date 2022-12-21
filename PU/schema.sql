6fe3531e1f05011ba4ce127ae975136632830473
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:05:02 +0100

    test of SQL export

---
+++ PU/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model PU of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE PU_type AS ENUM ('PU');
+CREATE TABLE PU (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type PU_type, unit text, value text);
