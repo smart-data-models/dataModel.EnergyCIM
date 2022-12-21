d7878f2c57559f63e2f84097d44b42c63027fba6
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:02:15 +0100

    test of SQL export

---
+++ LoadArea/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model LoadArea of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE LoadArea_type AS ENUM ('LoadArea');
+CREATE TABLE LoadArea (SubLoadAreas text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type LoadArea_type);
