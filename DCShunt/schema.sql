0746f9515e3fd24bf588b8670e7820c1db4f41c2
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:56:57 +0100

    test of SQL export

---
+++ DCShunt/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model DCShunt of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE DCShunt_type AS ENUM ('DCShunt');
+CREATE TABLE DCShunt (address json, alternateName text, areaServed text, capacitance text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, ratedUdc text, resistance text, seeAlso json, source text, type DCShunt_type);
