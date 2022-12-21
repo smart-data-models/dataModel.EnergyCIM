f4be85a12cb45c6c6e259486d0604674e111177a
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:02:06 +0100

    test of SQL export

---
+++ LimitSet/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model LimitSet of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE LimitSet_type AS ENUM ('LimitSet');
+CREATE TABLE LimitSet (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, isPercentageLimits text, location json, name text, owner json, seeAlso json, source text, type LimitSet_type);
