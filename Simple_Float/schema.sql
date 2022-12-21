1d90428506289679f4f44e8aa7627bcd45adbb53
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:06:01 +0100

    test of SQL export

---
+++ Simple_Float/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Simple_Float of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Simple_Float_type AS ENUM ('Simple_Float');
+CREATE TABLE Simple_Float (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type Simple_Float_type, value text);
