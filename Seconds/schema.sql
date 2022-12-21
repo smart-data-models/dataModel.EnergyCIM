60b01c13483e9d41b48864d750cb9a03c7d03c97
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:05:53 +0100

    test of SQL export

---
+++ Seconds/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model Seconds of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE Seconds_type AS ENUM ('Seconds');
+CREATE TABLE Seconds (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type Seconds_type, unit text, value text);
