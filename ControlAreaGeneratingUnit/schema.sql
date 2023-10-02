e1e20aa0bbffeb0c5e8bc5dbe05170cae2779884
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:37:25 +0200

    test of SQL export

--- ControlAreaGeneratingUnit/schema.sql
+++ ControlAreaGeneratingUnit/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ControlAreaGeneratingUnit of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ControlAreaGeneratingUnit of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ControlAreaGeneratingUnit_type AS ENUM ('ControlAreaGeneratingUnit');
-CREATE TABLE ControlAreaGeneratingUnit (ControlArea text, GeneratingUnit text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type ControlAreaGeneratingUnit_type);
+CREATE TABLE ControlAreaGeneratingUnit (ControlArea NUMERIC, GeneratingUnit NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type ControlAreaGeneratingUnit_type);
