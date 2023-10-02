e2dba77038346bfa740a3d365df2c67fb63bab64
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:47:13 +0200

    test of SQL export

--- RatioTapChanger/schema.sql
+++ RatioTapChanger/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model RatioTapChanger of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model RatioTapChanger of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE RatioTapChanger_type AS ENUM ('RatioTapChanger');
-CREATE TABLE RatioTapChanger (RatioTapChangerTable text, TransformerEnd text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, stepVoltageIncrement text, tculControlMode text, type RatioTapChanger_type);
+CREATE TABLE RatioTapChanger (RatioTapChangerTable NUMERIC, TransformerEnd NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, stepVoltageIncrement NUMERIC, tculControlMode NUMERIC, type RatioTapChanger_type);
