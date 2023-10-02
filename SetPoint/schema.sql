53a6716a32c16b22cba7c53c17310ca441b17fd5
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:48:08 +0200

    test of SQL export

--- SetPoint/schema.sql
+++ SetPoint/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model SetPoint of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model SetPoint of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SetPoint_type AS ENUM ('SetPoint');
-CREATE TABLE SetPoint (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, normalValue text, owner json, seeAlso json, source text, type SetPoint_type, value text);
+CREATE TABLE SetPoint (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, normalValue NUMERIC, owner JSON, source TEXT, type SetPoint_type, value NUMERIC);
