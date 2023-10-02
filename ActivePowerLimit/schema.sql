e6e539d9e87090609b8c77c51e1ce5c18d69cc1d
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:33:55 +0200

    test of SQL export

--- ActivePowerLimit/schema.sql
+++ ActivePowerLimit/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ActivePowerLimit of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ActivePowerLimit of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ActivePowerLimit_type AS ENUM ('ActivePowerLimit');
-CREATE TABLE ActivePowerLimit (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type ActivePowerLimit_type, value text);
+CREATE TABLE ActivePowerLimit (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type ActivePowerLimit_type, value NUMERIC);
