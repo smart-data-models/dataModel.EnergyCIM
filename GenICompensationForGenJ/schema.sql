4643fe18853bdaed38bc3a8030fba7cddf1f313c
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:42:00 +0200

    test of SQL export

--- GenICompensationForGenJ/schema.sql
+++ GenICompensationForGenJ/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model GenICompensationForGenJ of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model GenICompensationForGenJ of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE GenICompensationForGenJ_type AS ENUM ('GenICompensationForGenJ');
-CREATE TABLE GenICompensationForGenJ (SynchronousMachineDynamics text, VcompIEEEType2 text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, rcij text, seeAlso json, source text, type GenICompensationForGenJ_type, xcij text);
+CREATE TABLE GenICompensationForGenJ (SynchronousMachineDynamics NUMERIC, VcompIEEEType2 NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, rcij NUMERIC, source TEXT, type GenICompensationForGenJ_type, xcij NUMERIC);
