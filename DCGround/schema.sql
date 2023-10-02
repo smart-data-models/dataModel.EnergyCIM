83b780b2c8d8ee436ecb90e07862b98aff2211bd
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:37:54 +0200

    test of SQL export

--- DCGround/schema.sql
+++ DCGround/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model DCGround of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model DCGround of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DCGround_type AS ENUM ('DCGround');
-CREATE TABLE DCGround (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, inductance text, location json, name text, owner json, r text, seeAlso json, source text, type DCGround_type);
+CREATE TABLE DCGround (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, inductance NUMERIC, name TEXT, owner JSON, r NUMERIC, source TEXT, type DCGround_type);
