89c6285b2316461b5aba391bd0dcc63c58d3efc3
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:37:47 +0200

    test of SQL export

--- DCConductingEquipment/schema.sql
+++ DCConductingEquipment/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model DCConductingEquipment of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model DCConductingEquipment of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DCConductingEquipment_type AS ENUM ('DCConductingEquipment');
-CREATE TABLE DCConductingEquipment (DCTerminals text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type DCConductingEquipment_type);
+CREATE TABLE DCConductingEquipment (DCTerminals NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type DCConductingEquipment_type);
