2f1092371e04240031c843ca63dbbb73b2ed0b4d
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:45:24 +0200

    test of SQL export

--- PerLengthDCLineParameter/schema.sql
+++ PerLengthDCLineParameter/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model PerLengthDCLineParameter of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model PerLengthDCLineParameter of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PerLengthDCLineParameter_type AS ENUM ('PerLengthDCLineParameter');
-CREATE TABLE PerLengthDCLineParameter (DCLineSegments text, address json, alternateName text, areaServed text, capacitance text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, inductance text, location json, name text, owner json, resistance text, seeAlso json, source text, type PerLengthDCLineParameter_type);
+CREATE TABLE PerLengthDCLineParameter (DCLineSegments NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, capacitance NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, inductance NUMERIC, name TEXT, owner JSON, resistance NUMERIC, source TEXT, type PerLengthDCLineParameter_type);
