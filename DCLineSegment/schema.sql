b279867644e17731da307bd01754973da7480eee
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:37:59 +0200

    test of SQL export

--- DCLineSegment/schema.sql
+++ DCLineSegment/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model DCLineSegment of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model DCLineSegment of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DCLineSegment_type AS ENUM ('DCLineSegment');
-CREATE TABLE DCLineSegment (PerLengthParameter text, address json, alternateName text, areaServed text, capacitance text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, inductance text, length text, location json, name text, owner json, resistance text, seeAlso json, source text, type DCLineSegment_type);
+CREATE TABLE DCLineSegment (PerLengthParameter NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, capacitance NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, inductance NUMERIC, length NUMERIC, name TEXT, owner JSON, resistance NUMERIC, source TEXT, type DCLineSegment_type);
