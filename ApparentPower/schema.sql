98b9a70ed711a6e1f4d3c858187182662a01f5e9
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:34:22 +0200

    test of SQL export

--- ApparentPower/schema.sql
+++ ApparentPower/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ApparentPower of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ApparentPower of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ApparentPower_type AS ENUM ('ApparentPower');
-CREATE TABLE ApparentPower (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type ApparentPower_type, unit text, value text);
+CREATE TABLE ApparentPower (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type ApparentPower_type, unit NUMERIC, value NUMERIC);
