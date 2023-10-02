ad66eb0f1da6ebf5175cea5dbc42bbdc1feba52f
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:38:02 +0200

    test of SQL export

--- DCNode/schema.sql
+++ DCNode/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model DCNode of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model DCNode of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DCNode_type AS ENUM ('DCNode');
-CREATE TABLE DCNode (DCEquipmentContainer text, DCTerminals text, DCTopologicalNode text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type DCNode_type);
+CREATE TABLE DCNode (DCEquipmentContainer NUMERIC, DCTerminals NUMERIC, DCTopologicalNode NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type DCNode_type);
