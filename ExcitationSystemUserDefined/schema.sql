fe89bbcc6243c8fc64cbc69a566f3b810d8f0cc8
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:41:18 +0200

    test of SQL export

--- ExcitationSystemUserDefined/schema.sql
+++ ExcitationSystemUserDefined/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ExcitationSystemUserDefined of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ExcitationSystemUserDefined of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ExcitationSystemUserDefined_type AS ENUM ('ExcitationSystemUserDefined');
-CREATE TABLE ExcitationSystemUserDefined (ProprietaryParameterDynamics text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, proprietary text, seeAlso json, source text, type ExcitationSystemUserDefined_type);
+CREATE TABLE ExcitationSystemUserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, proprietary NUMERIC, source TEXT, type ExcitationSystemUserDefined_type);
