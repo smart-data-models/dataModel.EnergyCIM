06b45c9f458371edc283bd4746925e4415aab225
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:39:04 +0200

    test of SQL export

--- EnergySchedulingType/schema.sql
+++ EnergySchedulingType/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model EnergySchedulingType of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model EnergySchedulingType of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE EnergySchedulingType_type AS ENUM ('EnergySchedulingType');
-CREATE TABLE EnergySchedulingType (EnergySource text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type EnergySchedulingType_type);
+CREATE TABLE EnergySchedulingType (EnergySource NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type EnergySchedulingType_type);
