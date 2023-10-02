240600a68f1d7f6054c352b451706002000a04ff
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:45:55 +0200

    test of SQL export

--- PhaseTapChangerAsymmetrical/schema.sql
+++ PhaseTapChangerAsymmetrical/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model PhaseTapChangerAsymmetrical of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model PhaseTapChangerAsymmetrical of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PhaseTapChangerAsymmetrical_type AS ENUM ('PhaseTapChangerAsymmetrical');
-CREATE TABLE PhaseTapChangerAsymmetrical (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type PhaseTapChangerAsymmetrical_type, windingConnectionAngle text);
+CREATE TABLE PhaseTapChangerAsymmetrical (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type PhaseTapChangerAsymmetrical_type, windingConnectionAngle NUMERIC);
