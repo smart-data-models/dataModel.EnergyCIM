fea64f145eb69a9b794ece0ad238c8198aff92ae
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:49:53 +0200

    test of SQL export

--- TurbineLoadControllerUserDefined/schema.sql
+++ TurbineLoadControllerUserDefined/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model TurbineLoadControllerUserDefined of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model TurbineLoadControllerUserDefined of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE TurbineLoadControllerUserDefined_type AS ENUM ('TurbineLoadControllerUserDefined');
-CREATE TABLE TurbineLoadControllerUserDefined (ProprietaryParameterDynamics text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, proprietary text, seeAlso json, source text, type TurbineLoadControllerUserDefined_type);
+CREATE TABLE TurbineLoadControllerUserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, proprietary NUMERIC, source TEXT, type TurbineLoadControllerUserDefined_type);
