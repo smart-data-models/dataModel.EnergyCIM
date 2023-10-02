d4cdea7f68ee252eeac80c788d13b8ed0c782df5
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:47:33 +0200

    test of SQL export

--- RegulatingCondEq/schema.sql
+++ RegulatingCondEq/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model RegulatingCondEq of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model RegulatingCondEq of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE RegulatingCondEq_type AS ENUM ('RegulatingCondEq');
-CREATE TABLE RegulatingCondEq (RegulatingControl text, address json, alternateName text, areaServed text, controlEnabled text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type RegulatingCondEq_type);
+CREATE TABLE RegulatingCondEq (RegulatingControl NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, controlEnabled NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type RegulatingCondEq_type);
