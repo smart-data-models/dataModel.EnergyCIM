df986272091f3f3c422851acd88d3f017afc795f
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:33:37 +0200

    test of SQL export

--- AccumulatorLimitSet/schema.sql
+++ AccumulatorLimitSet/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model AccumulatorLimitSet of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model AccumulatorLimitSet of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE AccumulatorLimitSet_type AS ENUM ('AccumulatorLimitSet');
-CREATE TABLE AccumulatorLimitSet (Limits text, Measurements text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type AccumulatorLimitSet_type);
+CREATE TABLE AccumulatorLimitSet (Limits NUMERIC, Measurements NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type AccumulatorLimitSet_type);
