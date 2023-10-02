81ce541987e78999b7acda69b43920cadd9c5132
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:43:43 +0200

    test of SQL export

--- Inductance/schema.sql
+++ Inductance/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Inductance of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Inductance of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Inductance_type AS ENUM ('Inductance');
-CREATE TABLE Inductance (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type Inductance_type, unit text, value text);
+CREATE TABLE Inductance (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type Inductance_type, unit NUMERIC, value NUMERIC);
