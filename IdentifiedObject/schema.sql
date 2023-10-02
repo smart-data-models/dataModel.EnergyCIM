38c9629d172898fd94059368db80ee60434d840f
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:43:40 +0200

    test of SQL export

--- IdentifiedObject/schema.sql
+++ IdentifiedObject/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model IdentifiedObject of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model IdentifiedObject of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE IdentifiedObject_type AS ENUM ('IdentifiedObject');
-CREATE TABLE IdentifiedObject (DiagramObjects text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, energyIdentCodeEic text, id text, location json, mRID text, name text, owner json, seeAlso json, shortName text, source text, type IdentifiedObject_type);
+CREATE TABLE IdentifiedObject (DiagramObjects NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description NUMERIC, energyIdentCodeEic NUMERIC, mRID NUMERIC, name NUMERIC, owner JSON, shortName NUMERIC, source TEXT, type IdentifiedObject_type);
