4633520271f32f83620baded888f7e7619ea507e
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:35:08 +0200

    test of SQL export

--- Conductor/schema.sql
+++ Conductor/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Conductor of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Conductor of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Conductor_type AS ENUM ('Conductor');
-CREATE TABLE Conductor (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, length text, location json, name text, owner json, seeAlso json, source text, type Conductor_type);
+CREATE TABLE Conductor (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, length NUMERIC, name TEXT, owner JSON, source TEXT, type Conductor_type);
