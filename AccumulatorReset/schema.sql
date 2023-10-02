cc741c814bd65074574d7501c71e7855dea9062a
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:33:40 +0200

    test of SQL export

--- AccumulatorReset/schema.sql
+++ AccumulatorReset/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model AccumulatorReset of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model AccumulatorReset of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE AccumulatorReset_type AS ENUM ('AccumulatorReset');
-CREATE TABLE AccumulatorReset (AccumulatorValue text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type AccumulatorReset_type);
+CREATE TABLE AccumulatorReset (AccumulatorValue NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type AccumulatorReset_type);
