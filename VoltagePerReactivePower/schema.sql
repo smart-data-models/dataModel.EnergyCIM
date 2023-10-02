ec54b83064efec6c2bde4cae759883373a31995a
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:50:46 +0200

    test of SQL export

--- VoltagePerReactivePower/schema.sql
+++ VoltagePerReactivePower/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model VoltagePerReactivePower of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model VoltagePerReactivePower of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE VoltagePerReactivePower_type AS ENUM ('VoltagePerReactivePower');
-CREATE TABLE VoltagePerReactivePower (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, denominatorMultiplier text, denominatorUnit text, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type VoltagePerReactivePower_type, unit text, value text);
+CREATE TABLE VoltagePerReactivePower (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, denominatorMultiplier NUMERIC, denominatorUnit NUMERIC, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type VoltagePerReactivePower_type, unit NUMERIC, value NUMERIC);
