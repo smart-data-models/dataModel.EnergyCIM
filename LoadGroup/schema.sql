e505828ae4ceba5be20695bbffaeadc576694dc7
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:44:10 +0200

    test of SQL export

--- LoadGroup/schema.sql
+++ LoadGroup/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model LoadGroup of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model LoadGroup of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE LoadGroup_type AS ENUM ('LoadGroup');
-CREATE TABLE LoadGroup (SubLoadArea text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type LoadGroup_type);
+CREATE TABLE LoadGroup (SubLoadArea NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type LoadGroup_type);
