953ffb563db41b4abb1580df25ce1b7d8c70d230
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:50:51 +0200

    test of SQL export

--- VsCapabilityCurve/schema.sql
+++ VsCapabilityCurve/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model VsCapabilityCurve of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model VsCapabilityCurve of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE VsCapabilityCurve_type AS ENUM ('VsCapabilityCurve');
-CREATE TABLE VsCapabilityCurve (VsConverterDCSides text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type VsCapabilityCurve_type);
+CREATE TABLE VsCapabilityCurve (VsConverterDCSides NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type VsCapabilityCurve_type);
