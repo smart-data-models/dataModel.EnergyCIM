d657c5e05b92075bc7c94ecdf8a67df74171de82
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:46:10 +0200

    test of SQL export

--- PositionPoint/schema.sql
+++ PositionPoint/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model PositionPoint of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model PositionPoint of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PositionPoint_type AS ENUM ('PositionPoint');
-CREATE TABLE PositionPoint (Location text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, sequenceNumber text, source text, type PositionPoint_type, xPosition text, yPosition text, zPosition text);
+CREATE TABLE PositionPoint (Location NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, sequenceNumber NUMERIC, source TEXT, type PositionPoint_type, xPosition NUMERIC, yPosition NUMERIC, zPosition NUMERIC);
