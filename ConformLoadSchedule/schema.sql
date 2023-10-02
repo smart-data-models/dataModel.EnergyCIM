cf6dff16947fff515a59824679cb2d84da5ed752
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:35:15 +0200

    test of SQL export

--- ConformLoadSchedule/schema.sql
+++ ConformLoadSchedule/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ConformLoadSchedule of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ConformLoadSchedule of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ConformLoadSchedule_type AS ENUM ('ConformLoadSchedule');
-CREATE TABLE ConformLoadSchedule (ConformLoadGroup text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type ConformLoadSchedule_type);
+CREATE TABLE ConformLoadSchedule (ConformLoadGroup NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type ConformLoadSchedule_type);
