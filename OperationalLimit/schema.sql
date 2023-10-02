c5902946a27320a1eeffd0fcb8b149ca367f02a5
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:44:58 +0200

    test of SQL export

--- OperationalLimit/schema.sql
+++ OperationalLimit/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model OperationalLimit of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model OperationalLimit of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE OperationalLimit_type AS ENUM ('OperationalLimit');
-CREATE TABLE OperationalLimit (OperationalLimitSet text, OperationalLimitType text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type OperationalLimit_type);
+CREATE TABLE OperationalLimit (OperationalLimitSet NUMERIC, OperationalLimitType NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type OperationalLimit_type);
