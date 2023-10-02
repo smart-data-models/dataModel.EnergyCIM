d47a2e178d8226f1ed893efc9e419bb0a08ef8c8
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:38:45 +0200

    test of SQL export

--- Discrete/schema.sql
+++ Discrete/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Discrete of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Discrete of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Discrete_type AS ENUM ('Discrete');
-CREATE TABLE Discrete (DiscreteValues text, ValueAliasSet text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type Discrete_type);
+CREATE TABLE Discrete (DiscreteValues NUMERIC, ValueAliasSet NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type Discrete_type);
