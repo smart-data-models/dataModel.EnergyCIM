a4ef200bee8e2936fdf09332ff632f783a886edb
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:34:27 +0200

    test of SQL export

--- Area/schema.sql
+++ Area/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Area of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Area of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Area_type AS ENUM ('Area');
-CREATE TABLE Area (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type Area_type, unit text, value text);
+CREATE TABLE Area (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type Area_type, unit NUMERIC, value NUMERIC);
