fac54f4b6b785a4585ae77488393950aa3a908ce
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:48:28 +0200

    test of SQL export

--- SubGeographicalRegion/schema.sql
+++ SubGeographicalRegion/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model SubGeographicalRegion of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model SubGeographicalRegion of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SubGeographicalRegion_type AS ENUM ('SubGeographicalRegion');
-CREATE TABLE SubGeographicalRegion (DCLines text, Lines text, Region text, Substations text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type SubGeographicalRegion_type);
+CREATE TABLE SubGeographicalRegion (DCLines NUMERIC, Lines NUMERIC, Region NUMERIC, Substations NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type SubGeographicalRegion_type);
