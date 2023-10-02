8ec2dac386f35277e5149240c64cb73fabec2d55
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:51:20 +0200

    test of SQL export

--- WindGeneratingUnit/schema.sql
+++ WindGeneratingUnit/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model WindGeneratingUnit of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model WindGeneratingUnit of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE WindGeneratingUnit_type AS ENUM ('WindGeneratingUnit');
-CREATE TABLE WindGeneratingUnit (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type WindGeneratingUnit_type, windGenUnitType text);
+CREATE TABLE WindGeneratingUnit (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type WindGeneratingUnit_type, windGenUnitType NUMERIC);
