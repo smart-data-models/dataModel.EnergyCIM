e8be6ce407fe54b1ce67e880c3036d3c8812682f
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:50:44 +0200

    test of SQL export

--- VoltageLimit/schema.sql
+++ VoltageLimit/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model VoltageLimit of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model VoltageLimit of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE VoltageLimit_type AS ENUM ('VoltageLimit');
-CREATE TABLE VoltageLimit (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type VoltageLimit_type, value text);
+CREATE TABLE VoltageLimit (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type VoltageLimit_type, value NUMERIC);
