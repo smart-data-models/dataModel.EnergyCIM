2cea0ddf20a3d20693eba0eb76401f61b33b8fb7
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:44:22 +0200

    test of SQL export

--- Location/schema.sql
+++ Location/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Location of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Location of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Location_type AS ENUM ('Location');
-CREATE TABLE Location (CoordinateSystem text, PositionPoints text, PowerSystemResources text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type Location_type);
+CREATE TABLE Location (CoordinateSystem NUMERIC, PositionPoints NUMERIC, PowerSystemResources NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type Location_type);
