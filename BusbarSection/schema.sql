e439ab47f9ccba8e853752cfebd3cbd86203d91e
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:34:50 +0200

    test of SQL export

--- BusbarSection/schema.sql
+++ BusbarSection/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model BusbarSection of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model BusbarSection of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE BusbarSection_type AS ENUM ('BusbarSection');
-CREATE TABLE BusbarSection (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, ipMax text, location json, name text, owner json, seeAlso json, source text, type BusbarSection_type);
+CREATE TABLE BusbarSection (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, ipMax NUMERIC, name TEXT, owner JSON, source TEXT, type BusbarSection_type);
