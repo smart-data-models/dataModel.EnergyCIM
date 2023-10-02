c56267d25e2aa5e13a0342e9db710d24c227cefd
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:48:53 +0200

    test of SQL export

--- SwitchSchedule/schema.sql
+++ SwitchSchedule/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model SwitchSchedule of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model SwitchSchedule of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SwitchSchedule_type AS ENUM ('SwitchSchedule');
-CREATE TABLE SwitchSchedule (Switch text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type SwitchSchedule_type);
+CREATE TABLE SwitchSchedule (Switch NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type SwitchSchedule_type);
