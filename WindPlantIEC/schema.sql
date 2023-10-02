e9c59cd8613e6eb56ac015341fa36dc1aee99108
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:51:47 +0200

    test of SQL export

--- WindPlantIEC/schema.sql
+++ WindPlantIEC/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model WindPlantIEC of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model WindPlantIEC of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE WindPlantIEC_type AS ENUM ('WindPlantIEC');
-CREATE TABLE WindPlantIEC (WindPlantFreqPcontrolIEC text, WindPlantReactiveControlIEC text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type WindPlantIEC_type);
+CREATE TABLE WindPlantIEC (WindPlantFreqPcontrolIEC NUMERIC, WindPlantReactiveControlIEC NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type WindPlantIEC_type);
