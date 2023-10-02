d02c76ceb7cf7cef426d6c2a2982ea4084c7ddd5
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:46:12 +0200

    test of SQL export

--- PowerSystemResource/schema.sql
+++ PowerSystemResource/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model PowerSystemResource of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model PowerSystemResource of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PowerSystemResource_type AS ENUM ('PowerSystemResource');
-CREATE TABLE PowerSystemResource (Controls text, Location text, Measurements text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type PowerSystemResource_type);
+CREATE TABLE PowerSystemResource (Controls NUMERIC, Location NUMERIC, Measurements NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type PowerSystemResource_type);
