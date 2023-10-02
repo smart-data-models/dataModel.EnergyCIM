4bb75bb72cec768368ea8435c802d05538d2ebcb
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:49:28 +0200

    test of SQL export

--- ThermalGeneratingUnit/schema.sql
+++ ThermalGeneratingUnit/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ThermalGeneratingUnit of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ThermalGeneratingUnit of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ThermalGeneratingUnit_type AS ENUM ('ThermalGeneratingUnit');
-CREATE TABLE ThermalGeneratingUnit (FossilFuels text, address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type ThermalGeneratingUnit_type);
+CREATE TABLE ThermalGeneratingUnit (FossilFuels NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type ThermalGeneratingUnit_type);
