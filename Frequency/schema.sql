2a0dd9c591ddcf65e5e421753e1c697802f97b77
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:41:56 +0200

    test of SQL export

--- Frequency/schema.sql
+++ Frequency/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Frequency of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Frequency of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Frequency_type AS ENUM ('Frequency');
-CREATE TABLE Frequency (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, multiplier text, name text, owner json, seeAlso json, source text, type Frequency_type, unit text, value text);
+CREATE TABLE Frequency (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type Frequency_type, unit NUMERIC, value NUMERIC);
