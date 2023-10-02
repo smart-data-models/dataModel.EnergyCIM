9c0026e089088b9e95d4b2b1d57aeec5024168be
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:39:39 +0200

    test of SQL export

--- ExcAC4A/schema.sql
+++ ExcAC4A/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ExcAC4A of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ExcAC4A of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ExcAC4A_type AS ENUM ('ExcAC4A');
-CREATE TABLE ExcAC4A (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, ka text, kc text, location json, name text, owner json, seeAlso json, source text, ta text, tb text, tc text, type ExcAC4A_type, vimax text, vimin text, vrmax text, vrmin text);
+CREATE TABLE ExcAC4A (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, ka NUMERIC, kc NUMERIC, name TEXT, owner JSON, source TEXT, ta NUMERIC, tb NUMERIC, tc NUMERIC, type ExcAC4A_type, vimax NUMERIC, vimin NUMERIC, vrmax NUMERIC, vrmin NUMERIC);
