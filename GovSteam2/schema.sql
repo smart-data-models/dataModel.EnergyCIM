32d6e346b15e22dc79b6596ea774133e3330365b
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:43:09 +0200

    test of SQL export

--- GovSteam2/schema.sql
+++ GovSteam2/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model GovSteam2 of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model GovSteam2 of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE GovSteam2_type AS ENUM ('GovSteam2');
-CREATE TABLE GovSteam2 (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, dbf text, description text, id text, k text, location json, mnef text, mxef text, name text, owner json, pmax text, pmin text, seeAlso json, source text, t1 text, t2 text, type GovSteam2_type);
+CREATE TABLE GovSteam2 (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, dbf NUMERIC, description TEXT, k NUMERIC, mnef NUMERIC, mxef NUMERIC, name TEXT, owner JSON, pmax NUMERIC, pmin NUMERIC, source TEXT, t1 NUMERIC, t2 NUMERIC, type GovSteam2_type);
