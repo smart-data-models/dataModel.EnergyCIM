95f19f349140b4d78ad2abf62e7753b88980c00c
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:39:57 +0200

    test of SQL export

--- ExcAVR3/schema.sql
+++ ExcAVR3/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ExcAVR3 of the subject dataModel.EnergyCIM for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ExcAVR3 of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ExcAVR3_type AS ENUM ('ExcAVR3');
-CREATE TABLE ExcAVR3 (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, e1 text, e2 text, id text, ka text, location json, name text, owner json, se1 text, se2 text, seeAlso json, source text, t1 text, t2 text, t3 text, t4 text, te text, type ExcAVR3_type, vrmn text, vrmx text);
+CREATE TABLE ExcAVR3 (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, e1 NUMERIC, e2 NUMERIC, ka NUMERIC, name TEXT, owner JSON, se1 NUMERIC, se2 NUMERIC, source TEXT, t1 NUMERIC, t2 NUMERIC, t3 NUMERIC, t4 NUMERIC, te NUMERIC, type ExcAVR3_type, vrmn NUMERIC, vrmx NUMERIC);
