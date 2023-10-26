bee92e14cbe3d3e33a2807a396688ff758aeef33
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:02:35 +0200

    test of SQL export

--- WindPlantDynamics/schema.sql
+++ WindPlantDynamics/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model WindPlantDynamics of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE WindPlantDynamics_type AS ENUM ('WindPlantDynamics');
-CREATE TABLE WindPlantDynamics (RemoteInputSignal NUMERIC, WindTurbineType3or4Dynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type WindPlantDynamics_type);
+CREATE TABLE WindPlantDynamics (RemoteInputSignal NUMERIC, WindTurbineType3or4Dynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type WindPlantDynamics_type);
