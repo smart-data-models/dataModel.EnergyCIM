0e7cd6a0f7fd9b983b19160eb0dcea4cb3fac5f8
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:53:05 +0200

    test of SQL export

--- SubLoadArea/schema.sql
+++ SubLoadArea/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model SubLoadArea of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SubLoadArea_type AS ENUM ('SubLoadArea');
-CREATE TABLE SubLoadArea (LoadArea NUMERIC, LoadGroups NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type SubLoadArea_type);
+CREATE TABLE SubLoadArea (LoadArea NUMERIC, LoadGroups NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type SubLoadArea_type);
