b7ac798ee69eec9d1b27be6c9fec7b35988aa81a
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:06:50 +0200

    test of SQL export

--- LinearShuntCompensator/schema.sql
+++ LinearShuntCompensator/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model LinearShuntCompensator of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE LinearShuntCompensator_type AS ENUM ('LinearShuntCompensator');
-CREATE TABLE LinearShuntCompensator (address JSON, alternateName TEXT, areaServed TEXT, b0PerSection NUMERIC, bPerSection NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, g0PerSection NUMERIC, gPerSection NUMERIC, name TEXT, owner JSON, source TEXT, type LinearShuntCompensator_type);
+CREATE TABLE LinearShuntCompensator (address JSON, alternateName TEXT, areaServed TEXT, b0PerSection NUMERIC, bPerSection NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, g0PerSection NUMERIC, gPerSection NUMERIC, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type LinearShuntCompensator_type);
