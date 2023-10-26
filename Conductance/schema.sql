19785427d6d91a7a907bb69229c5a856645899a7
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:00:11 +0200

    test of SQL export

--- Conductance/schema.sql
+++ Conductance/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Conductance of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Conductance_type AS ENUM ('Conductance');
-CREATE TABLE Conductance (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type Conductance_type, unit NUMERIC, value NUMERIC);
+CREATE TABLE Conductance (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, multiplier NUMERIC, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Conductance_type, unit NUMERIC, value NUMERIC);
