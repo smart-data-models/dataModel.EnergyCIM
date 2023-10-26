00644c76a5dbdbd0de427415c0133c68bca4fb6a
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:00:08 +0200

    test of SQL export

--- Command/schema.sql
+++ Command/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Command of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Command_type AS ENUM ('Command');
-CREATE TABLE Command (DiscreteValue NUMERIC, ValueAliasSet NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, normalValue NUMERIC, owner JSON, source TEXT, type Command_type, value NUMERIC);
+CREATE TABLE Command (DiscreteValue NUMERIC, ValueAliasSet NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, normalValue NUMERIC, owner JSON, seeAlso JSON, source TEXT, type Command_type, value NUMERIC);
