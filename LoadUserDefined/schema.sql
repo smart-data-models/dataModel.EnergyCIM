dcb5319abc0c2b44599ac50a89cee1ea04461ae9
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:07:14 +0200

    test of SQL export

--- LoadUserDefined/schema.sql
+++ LoadUserDefined/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model LoadUserDefined of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE LoadUserDefined_type AS ENUM ('LoadUserDefined');
-CREATE TABLE LoadUserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, proprietary NUMERIC, source TEXT, type LoadUserDefined_type);
+CREATE TABLE LoadUserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, proprietary NUMERIC, seeAlso JSON, source TEXT, type LoadUserDefined_type);
