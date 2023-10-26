31f6fa7015b1d4605da2c2bfb17c4f11cdb6959f
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:55:00 +0200

    test of SQL export

--- VisibilityLayer/schema.sql
+++ VisibilityLayer/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model VisibilityLayer of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE VisibilityLayer_type AS ENUM ('VisibilityLayer');
-CREATE TABLE VisibilityLayer (VisibleObjects NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, drawingOrder NUMERIC, name TEXT, owner JSON, source TEXT, type VisibilityLayer_type);
+CREATE TABLE VisibilityLayer (VisibleObjects NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, drawingOrder NUMERIC, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type VisibilityLayer_type);
