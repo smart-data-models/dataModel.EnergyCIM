a670e39bac42f88eda27f92aafa881a7ea1b996d
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:51:40 +0200

    test of SQL export

--- PssSK/schema.sql
+++ PssSK/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model PssSK of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PssSK_type AS ENUM ('PssSK');
-CREATE TABLE PssSK (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, k1 NUMERIC, k2 NUMERIC, k3 NUMERIC, name TEXT, owner JSON, source TEXT, t1 NUMERIC, t2 NUMERIC, t3 NUMERIC, t4 NUMERIC, t5 NUMERIC, t6 NUMERIC, type PssSK_type, vsmax NUMERIC, vsmin NUMERIC);
+CREATE TABLE PssSK (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, k1 NUMERIC, k2 NUMERIC, k3 NUMERIC, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, t1 NUMERIC, t2 NUMERIC, t3 NUMERIC, t4 NUMERIC, t5 NUMERIC, t6 NUMERIC, type PssSK_type, vsmax NUMERIC, vsmin NUMERIC);
