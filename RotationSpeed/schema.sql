3ab39cee136edda66d8c9620793a5cdfa4695a90
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:52:32 +0200

    test of SQL export

--- RotationSpeed/schema.sql
+++ RotationSpeed/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model RotationSpeed of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE RotationSpeed_type AS ENUM ('RotationSpeed');
-CREATE TABLE RotationSpeed (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, denominatorMultiplier NUMERIC, denominatorUnit NUMERIC, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type RotationSpeed_type, unit NUMERIC, value NUMERIC);
+CREATE TABLE RotationSpeed (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, denominatorMultiplier NUMERIC, denominatorUnit NUMERIC, description TEXT, id TEXT PRIMARY KEY, location JSON, multiplier NUMERIC, name TEXT, owner JSON, seeAlso JSON, source TEXT, type RotationSpeed_type, unit NUMERIC, value NUMERIC);
