7d16b49d5b3c13e3db582a6ffcf1cc75c29a2fe2
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:59:28 +0200

    test of SQL export

--- AngleDegrees/schema.sql
+++ AngleDegrees/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model AngleDegrees of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE AngleDegrees_type AS ENUM ('AngleDegrees');
-CREATE TABLE AngleDegrees (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type AngleDegrees_type, unit NUMERIC, value NUMERIC);
+CREATE TABLE AngleDegrees (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, multiplier NUMERIC, name TEXT, owner JSON, seeAlso JSON, source TEXT, type AngleDegrees_type, unit NUMERIC, value NUMERIC);
