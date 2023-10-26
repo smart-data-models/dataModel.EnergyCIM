8d7e4d6a6154c9fc5c96b321b49b1aa6515cf23a
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:52:37 +0200

    test of SQL export

--- SeasonDayTypeSchedule/schema.sql
+++ SeasonDayTypeSchedule/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model SeasonDayTypeSchedule of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SeasonDayTypeSchedule_type AS ENUM ('SeasonDayTypeSchedule');
-CREATE TABLE SeasonDayTypeSchedule (DayType NUMERIC, Season NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type SeasonDayTypeSchedule_type);
+CREATE TABLE SeasonDayTypeSchedule (DayType NUMERIC, Season NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type SeasonDayTypeSchedule_type);
