7d07befd0efcb8839eeec8b9461f3f02c3509daf
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:53:17 +0200

    test of SQL export

--- SvShuntCompensatorSections/schema.sql
+++ SvShuntCompensatorSections/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model SvShuntCompensatorSections of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SvShuntCompensatorSections_type AS ENUM ('SvShuntCompensatorSections');
-CREATE TABLE SvShuntCompensatorSections (ShuntCompensator NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, sections NUMERIC, source TEXT, type SvShuntCompensatorSections_type);
+CREATE TABLE SvShuntCompensatorSections (ShuntCompensator NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, sections NUMERIC, seeAlso JSON, source TEXT, type SvShuntCompensatorSections_type);
