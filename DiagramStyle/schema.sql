fdab7d6241cea425d4da68a1f3e26910936b3bf7
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:01:37 +0200

    test of SQL export

--- DiagramStyle/schema.sql
+++ DiagramStyle/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DiagramStyle of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DiagramStyle_type AS ENUM ('DiagramStyle');
-CREATE TABLE DiagramStyle (Diagram NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type DiagramStyle_type);
+CREATE TABLE DiagramStyle (Diagram NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type DiagramStyle_type);
