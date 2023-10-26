e650ce304df5b6fb13a91d4715148040d4dac928
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:06:43 +0200

    test of SQL export

--- Length/schema.sql
+++ Length/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Length of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Length_type AS ENUM ('Length');
-CREATE TABLE Length (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type Length_type, unit NUMERIC, value NUMERIC);
+CREATE TABLE Length (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, multiplier NUMERIC, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Length_type, unit NUMERIC, value NUMERIC);
