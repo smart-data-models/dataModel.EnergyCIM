0f9cabbc4a7f0252a5feef6918a3a7dfedadc7c8
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:55:02 +0200

    test of SQL export

--- Voltage/schema.sql
+++ Voltage/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Voltage of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Voltage_type AS ENUM ('Voltage');
-CREATE TABLE Voltage (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type Voltage_type, unit NUMERIC, value NUMERIC);
+CREATE TABLE Voltage (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, multiplier NUMERIC, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Voltage_type, unit NUMERIC, value NUMERIC);
