5e4a8eeec4187cbe7c91b99e0fbecd272e9974c0
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:07:58 +0200

    test of SQL export

--- OverexcitationLimiterDynamics/schema.sql
+++ OverexcitationLimiterDynamics/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model OverexcitationLimiterDynamics of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE OverexcitationLimiterDynamics_type AS ENUM ('OverexcitationLimiterDynamics');
-CREATE TABLE OverexcitationLimiterDynamics (ExcitationSystemDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type OverexcitationLimiterDynamics_type);
+CREATE TABLE OverexcitationLimiterDynamics (ExcitationSystemDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type OverexcitationLimiterDynamics_type);
