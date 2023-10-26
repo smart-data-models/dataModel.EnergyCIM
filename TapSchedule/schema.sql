63a0fb1c7c4e3a9590f3eab8ab012286701d04c3
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:53:52 +0200

    test of SQL export

--- TapSchedule/schema.sql
+++ TapSchedule/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model TapSchedule of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE TapSchedule_type AS ENUM ('TapSchedule');
-CREATE TABLE TapSchedule (TapChanger NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type TapSchedule_type);
+CREATE TABLE TapSchedule (TapChanger NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type TapSchedule_type);
