17369864a814c695f55ce9918a268094e8e71b7b
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:52:22 +0200

    test of SQL export

--- Resistance/schema.sql
+++ Resistance/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Resistance of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Resistance_type AS ENUM ('Resistance');
-CREATE TABLE Resistance (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type Resistance_type, unit NUMERIC, value NUMERIC);
+CREATE TABLE Resistance (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, multiplier NUMERIC, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Resistance_type, unit NUMERIC, value NUMERIC);
