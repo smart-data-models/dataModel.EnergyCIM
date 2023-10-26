09655e28165e4ebcac515805b538aa694e8b974f
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:55:07 +0200

    test of SQL export

--- VoltageAdjusterUserDefined/schema.sql
+++ VoltageAdjusterUserDefined/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model VoltageAdjusterUserDefined of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE VoltageAdjusterUserDefined_type AS ENUM ('VoltageAdjusterUserDefined');
-CREATE TABLE VoltageAdjusterUserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, proprietary NUMERIC, source TEXT, type VoltageAdjusterUserDefined_type);
+CREATE TABLE VoltageAdjusterUserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, proprietary NUMERIC, seeAlso JSON, source TEXT, type VoltageAdjusterUserDefined_type);
