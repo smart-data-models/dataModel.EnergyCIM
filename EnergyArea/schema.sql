114fe4d2646a4c70710ec760d874cac3fcbe7dcb
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:02:03 +0200

    test of SQL export

--- EnergyArea/schema.sql
+++ EnergyArea/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model EnergyArea of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE EnergyArea_type AS ENUM ('EnergyArea');
-CREATE TABLE EnergyArea (ControlArea NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type EnergyArea_type);
+CREATE TABLE EnergyArea (ControlArea NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type EnergyArea_type);
