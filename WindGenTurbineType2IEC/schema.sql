84714279708d252153a5a5094157a6e1bbe39d86
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:02:18 +0200

    test of SQL export

--- WindGenTurbineType2IEC/schema.sql
+++ WindGenTurbineType2IEC/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model WindGenTurbineType2IEC of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE WindGenTurbineType2IEC_type AS ENUM ('WindGenTurbineType2IEC');
-CREATE TABLE WindGenTurbineType2IEC (WindContRotorRIEC NUMERIC, WindPitchContEmulIEC NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type WindGenTurbineType2IEC_type);
+CREATE TABLE WindGenTurbineType2IEC (WindContRotorRIEC NUMERIC, WindPitchContEmulIEC NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type WindGenTurbineType2IEC_type);
