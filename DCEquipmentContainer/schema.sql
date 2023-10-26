4e8c530fda9c672bf5236dd5f4f1987d85b09078
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:00:59 +0200

    test of SQL export

--- DCEquipmentContainer/schema.sql
+++ DCEquipmentContainer/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DCEquipmentContainer of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DCEquipmentContainer_type AS ENUM ('DCEquipmentContainer');
-CREATE TABLE DCEquipmentContainer (DCNodes NUMERIC, DCTopologicalNode NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type DCEquipmentContainer_type);
+CREATE TABLE DCEquipmentContainer (DCNodes NUMERIC, DCTopologicalNode NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type DCEquipmentContainer_type);
