ecf6bfbff27f6a3fdadb49c2fcc84f5a5d452794
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:00:27 +0200

    test of SQL export

--- ConnectivityNodeContainer/schema.sql
+++ ConnectivityNodeContainer/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ConnectivityNodeContainer of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ConnectivityNodeContainer_type AS ENUM ('ConnectivityNodeContainer');
-CREATE TABLE ConnectivityNodeContainer (ConnectivityNodes NUMERIC, TopologicalNode NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type ConnectivityNodeContainer_type);
+CREATE TABLE ConnectivityNodeContainer (ConnectivityNodes NUMERIC, TopologicalNode NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type ConnectivityNodeContainer_type);
