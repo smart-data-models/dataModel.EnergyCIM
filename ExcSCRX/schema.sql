271b698fd3a01aaad1a7f04e956741c260b0c58e
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:04:28 +0200

    test of SQL export

--- ExcSCRX/schema.sql
+++ ExcSCRX/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ExcSCRX of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ExcSCRX_type AS ENUM ('ExcSCRX');
-CREATE TABLE ExcSCRX (address JSON, alternateName TEXT, areaServed TEXT, cswitch NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, emax NUMERIC, emin NUMERIC, k NUMERIC, name TEXT, owner JSON, rcrfd NUMERIC, source TEXT, tatb NUMERIC, tb NUMERIC, te NUMERIC, type ExcSCRX_type);
+CREATE TABLE ExcSCRX (address JSON, alternateName TEXT, areaServed TEXT, cswitch NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, emax NUMERIC, emin NUMERIC, id TEXT PRIMARY KEY, k NUMERIC, location JSON, name TEXT, owner JSON, rcrfd NUMERIC, seeAlso JSON, source TEXT, tatb NUMERIC, tb NUMERIC, te NUMERIC, type ExcSCRX_type);
