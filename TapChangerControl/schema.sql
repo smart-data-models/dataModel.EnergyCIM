c4166667e7339cad0dea51dadd5b41bd24194687
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:53:47 +0200

    test of SQL export

--- TapChangerControl/schema.sql
+++ TapChangerControl/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model TapChangerControl of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE TapChangerControl_type AS ENUM ('TapChangerControl');
-CREATE TABLE TapChangerControl (TapChanger NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type TapChangerControl_type);
+CREATE TABLE TapChangerControl (TapChanger NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type TapChangerControl_type);
