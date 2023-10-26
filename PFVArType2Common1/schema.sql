7cf815a91e6e5bb260d5897a428caab3bded21b3
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:50:24 +0200

    test of SQL export

--- PFVArType2Common1/schema.sql
+++ PFVArType2Common1/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model PFVArType2Common1 of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PFVArType2Common1_type AS ENUM ('PFVArType2Common1');
-CREATE TABLE PFVArType2Common1 (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, j NUMERIC, ki NUMERIC, kp NUMERIC, max NUMERIC, name TEXT, owner JSON, ref NUMERIC, source TEXT, type PFVArType2Common1_type);
+CREATE TABLE PFVArType2Common1 (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, j NUMERIC, ki NUMERIC, kp NUMERIC, location JSON, max NUMERIC, name TEXT, owner JSON, ref NUMERIC, seeAlso JSON, source TEXT, type PFVArType2Common1_type);
