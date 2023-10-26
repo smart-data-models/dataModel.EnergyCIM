dc672ff01bdc08991ebd2ca3898c2a7d2ad27a0b
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:55:05 +0200

    test of SQL export

--- VoltageAdjusterDynamics/schema.sql
+++ VoltageAdjusterDynamics/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model VoltageAdjusterDynamics of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE VoltageAdjusterDynamics_type AS ENUM ('VoltageAdjusterDynamics');
-CREATE TABLE VoltageAdjusterDynamics (PFVArControllerType1Dynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type VoltageAdjusterDynamics_type);
+CREATE TABLE VoltageAdjusterDynamics (PFVArControllerType1Dynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type VoltageAdjusterDynamics_type);
