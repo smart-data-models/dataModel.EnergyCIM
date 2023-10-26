829334726d1d8c79931fbb80d0bce11562602f32
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:59:16 +0200

    test of SQL export

--- Analog/schema.sql
+++ Analog/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Analog of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Analog_type AS ENUM ('Analog');
-CREATE TABLE Analog (AnalogValues NUMERIC, LimitSets NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, positiveFlowIn NUMERIC, source TEXT, type Analog_type);
+CREATE TABLE Analog (AnalogValues NUMERIC, LimitSets NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, positiveFlowIn NUMERIC, seeAlso JSON, source TEXT, type Analog_type);
