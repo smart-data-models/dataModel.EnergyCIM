bb04a6902eef36bd4c82c4985d64cdfc6dc85474
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:54:51 +0200

    test of SQL export

--- ValueAliasSet/schema.sql
+++ ValueAliasSet/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ValueAliasSet of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ValueAliasSet_type AS ENUM ('ValueAliasSet');
-CREATE TABLE ValueAliasSet (Commands NUMERIC, Discretes NUMERIC, RaiseLowerCommands NUMERIC, Values NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type ValueAliasSet_type);
+CREATE TABLE ValueAliasSet (Commands NUMERIC, Discretes NUMERIC, RaiseLowerCommands NUMERIC, Values NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type ValueAliasSet_type);
