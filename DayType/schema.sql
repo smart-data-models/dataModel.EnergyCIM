695abb0f032180aabaf3025e169da172032cc7de
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:00:49 +0200

    test of SQL export

--- DayType/schema.sql
+++ DayType/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DayType of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DayType_type AS ENUM ('DayType');
-CREATE TABLE DayType (SeasonDayTypeSchedules NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type DayType_type);
+CREATE TABLE DayType (SeasonDayTypeSchedules NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type DayType_type);
