00692352b1eda3cd0d141d814aa6e9ecd93988c1
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:06:29 +0200

    test of SQL export

--- HydroGeneratingUnit/schema.sql
+++ HydroGeneratingUnit/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model HydroGeneratingUnit of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE HydroGeneratingUnit_type AS ENUM ('HydroGeneratingUnit');
-CREATE TABLE HydroGeneratingUnit (HydroPowerPlant NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, energyConversionCapability NUMERIC, name TEXT, owner JSON, source TEXT, type HydroGeneratingUnit_type);
+CREATE TABLE HydroGeneratingUnit (HydroPowerPlant NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, energyConversionCapability NUMERIC, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type HydroGeneratingUnit_type);
