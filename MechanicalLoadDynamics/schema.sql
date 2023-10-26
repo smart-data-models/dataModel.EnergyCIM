f8a2f63493d67f1ab72b9546dc8ea64014dd9023
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:07:28 +0200

    test of SQL export

--- MechanicalLoadDynamics/schema.sql
+++ MechanicalLoadDynamics/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model MechanicalLoadDynamics of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE MechanicalLoadDynamics_type AS ENUM ('MechanicalLoadDynamics');
-CREATE TABLE MechanicalLoadDynamics (AsynchronousMachineDynamics NUMERIC, SynchronousMachineDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type MechanicalLoadDynamics_type);
+CREATE TABLE MechanicalLoadDynamics (AsynchronousMachineDynamics NUMERIC, SynchronousMachineDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type MechanicalLoadDynamics_type);
