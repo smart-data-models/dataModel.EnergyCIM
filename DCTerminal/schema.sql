5945b64a945b9463aab1cdc0befe2f25d0f4e97a
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:01:16 +0200

    test of SQL export

--- DCTerminal/schema.sql
+++ DCTerminal/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DCTerminal of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DCTerminal_type AS ENUM ('DCTerminal');
-CREATE TABLE DCTerminal (DCConductingEquipment NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type DCTerminal_type);
+CREATE TABLE DCTerminal (DCConductingEquipment NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type DCTerminal_type);
