0b42275bd98ef99706ee6c4b54807801bba2ed0a
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:53:59 +0200

    test of SQL export

--- TextDiagramObject/schema.sql
+++ TextDiagramObject/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model TextDiagramObject of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE TextDiagramObject_type AS ENUM ('TextDiagramObject');
-CREATE TABLE TextDiagramObject (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, text NUMERIC, type TextDiagramObject_type);
+CREATE TABLE TextDiagramObject (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, text NUMERIC, type TextDiagramObject_type);
