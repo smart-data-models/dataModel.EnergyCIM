88253660c5e86dd51d3642eac3546235ac353816
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:50:37 +0200

    test of SQL export

--- PhaseTapChangerLinear/schema.sql
+++ PhaseTapChangerLinear/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model PhaseTapChangerLinear of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PhaseTapChangerLinear_type AS ENUM ('PhaseTapChangerLinear');
-CREATE TABLE PhaseTapChangerLinear (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, stepPhaseShiftIncrement NUMERIC, type PhaseTapChangerLinear_type, xMax NUMERIC, xMin NUMERIC);
+CREATE TABLE PhaseTapChangerLinear (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, stepPhaseShiftIncrement NUMERIC, type PhaseTapChangerLinear_type, xMax NUMERIC, xMin NUMERIC);
