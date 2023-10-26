73be49a9bf7d670cb3d4f192117e6161dcc3ceaf
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:02:32 +0200

    test of SQL export

--- EquivalentShunt/schema.sql
+++ EquivalentShunt/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model EquivalentShunt of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE EquivalentShunt_type AS ENUM ('EquivalentShunt');
-CREATE TABLE EquivalentShunt (address JSON, alternateName TEXT, areaServed TEXT, b NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, g NUMERIC, name TEXT, owner JSON, source TEXT, type EquivalentShunt_type);
+CREATE TABLE EquivalentShunt (address JSON, alternateName TEXT, areaServed TEXT, b NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, g NUMERIC, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type EquivalentShunt_type);
