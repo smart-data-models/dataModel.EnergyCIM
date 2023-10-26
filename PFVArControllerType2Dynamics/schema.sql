184eb90940baddaa899ed4e7e214a8ffd784779e
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:50:14 +0200

    test of SQL export

--- PFVArControllerType2Dynamics/schema.sql
+++ PFVArControllerType2Dynamics/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model PFVArControllerType2Dynamics of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PFVArControllerType2Dynamics_type AS ENUM ('PFVArControllerType2Dynamics');
-CREATE TABLE PFVArControllerType2Dynamics (ExcitationSystemDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type PFVArControllerType2Dynamics_type);
+CREATE TABLE PFVArControllerType2Dynamics (ExcitationSystemDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type PFVArControllerType2Dynamics_type);
