d5e4766511e91e328daee54014612af6dda55ea5
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:54:05 +0200

    test of SQL export

--- TieFlow/schema.sql
+++ TieFlow/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model TieFlow of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE TieFlow_type AS ENUM ('TieFlow');
-CREATE TABLE TieFlow (ControlArea NUMERIC, Terminal NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, positiveFlowIn NUMERIC, source TEXT, type TieFlow_type);
+CREATE TABLE TieFlow (ControlArea NUMERIC, Terminal NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, positiveFlowIn NUMERIC, seeAlso JSON, source TEXT, type TieFlow_type);
