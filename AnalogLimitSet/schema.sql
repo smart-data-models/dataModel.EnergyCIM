0ba95a414a3af611df8b09392bef0b08822ad432
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:59:23 +0200

    test of SQL export

--- AnalogLimitSet/schema.sql
+++ AnalogLimitSet/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model AnalogLimitSet of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE AnalogLimitSet_type AS ENUM ('AnalogLimitSet');
-CREATE TABLE AnalogLimitSet (Limits NUMERIC, Measurements NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type AnalogLimitSet_type);
+CREATE TABLE AnalogLimitSet (Limits NUMERIC, Measurements NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type AnalogLimitSet_type);
