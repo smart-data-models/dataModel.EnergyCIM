c0a74bf31c6dbde75eba3d031f1b7904c7bf64dd
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:00:57 +0200

    test of SQL export

--- DCConverterUnit/schema.sql
+++ DCConverterUnit/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DCConverterUnit of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DCConverterUnit_type AS ENUM ('DCConverterUnit');
-CREATE TABLE DCConverterUnit (Substation NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, operationMode NUMERIC, owner JSON, source TEXT, type DCConverterUnit_type);
+CREATE TABLE DCConverterUnit (Substation NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, operationMode NUMERIC, owner JSON, seeAlso JSON, source TEXT, type DCConverterUnit_type);
