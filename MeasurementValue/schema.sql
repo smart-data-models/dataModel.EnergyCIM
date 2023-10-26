8bccb04faccc7bd671c45e820e633015ae6f670f
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:07:21 +0200

    test of SQL export

--- MeasurementValue/schema.sql
+++ MeasurementValue/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model MeasurementValue of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE MeasurementValue_type AS ENUM ('MeasurementValue');
-CREATE TABLE MeasurementValue (MeasurementValueQuality NUMERIC, MeasurementValueSource NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, sensorAccuracy NUMERIC, source TEXT, timeStamp NUMERIC, type MeasurementValue_type);
+CREATE TABLE MeasurementValue (MeasurementValueQuality NUMERIC, MeasurementValueSource NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, sensorAccuracy NUMERIC, source TEXT, timeStamp NUMERIC, type MeasurementValue_type);
