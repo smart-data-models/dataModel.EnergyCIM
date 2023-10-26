023cb687c7ca48f950e75efded7f00ec5d2bd81e
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:04:51 +0200

    test of SQL export

--- FossilFuel/schema.sql
+++ FossilFuel/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model FossilFuel of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE FossilFuel_type AS ENUM ('FossilFuel');
-CREATE TABLE FossilFuel (ThermalGeneratingUnit NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, fossilFuelType NUMERIC, name TEXT, owner JSON, source TEXT, type FossilFuel_type);
+CREATE TABLE FossilFuel (ThermalGeneratingUnit NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, fossilFuelType NUMERIC, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type FossilFuel_type);
