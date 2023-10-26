6b0aee7835f6aa9f014ea45282ca7c632950067a
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:03:11 +0200

    test of SQL export

--- ExcCZ/schema.sql
+++ ExcCZ/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ExcCZ of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ExcCZ_type AS ENUM ('ExcCZ');
-CREATE TABLE ExcCZ (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, efdmax NUMERIC, efdmin NUMERIC, ka NUMERIC, ke NUMERIC, kp NUMERIC, name TEXT, owner JSON, source TEXT, ta NUMERIC, tc NUMERIC, te NUMERIC, type ExcCZ_type, vrmax NUMERIC, vrmin NUMERIC);
+CREATE TABLE ExcCZ (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, efdmax NUMERIC, efdmin NUMERIC, id TEXT PRIMARY KEY, ka NUMERIC, ke NUMERIC, kp NUMERIC, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, ta NUMERIC, tc NUMERIC, te NUMERIC, type ExcCZ_type, vrmax NUMERIC, vrmin NUMERIC);
