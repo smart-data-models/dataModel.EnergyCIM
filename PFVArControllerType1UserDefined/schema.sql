b7e22c5af1f444c8ecfb8fb93dd6ebad24b34632
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:50:12 +0200

    test of SQL export

--- PFVArControllerType1UserDefined/schema.sql
+++ PFVArControllerType1UserDefined/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model PFVArControllerType1UserDefined of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PFVArControllerType1UserDefined_type AS ENUM ('PFVArControllerType1UserDefined');
-CREATE TABLE PFVArControllerType1UserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, proprietary NUMERIC, source TEXT, type PFVArControllerType1UserDefined_type);
+CREATE TABLE PFVArControllerType1UserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, proprietary NUMERIC, seeAlso JSON, source TEXT, type PFVArControllerType1UserDefined_type);
