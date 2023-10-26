d2adb730834f3efae38dd5b381658735940e2e70
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:00:45 +0200

    test of SQL export

--- Curve/schema.sql
+++ Curve/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Curve of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Curve_type AS ENUM ('Curve');
-CREATE TABLE Curve (CurveDatas NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, curveStyle NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type Curve_type, xUnit NUMERIC, y1Unit NUMERIC, y2Unit NUMERIC);
+CREATE TABLE Curve (CurveDatas NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, curveStyle NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Curve_type, xUnit NUMERIC, y1Unit NUMERIC, y2Unit NUMERIC);
