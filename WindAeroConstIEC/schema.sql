accc56becd52b80ee9b5228db7771b6069ea4622
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:55:29 +0200

    test of SQL export

--- WindAeroConstIEC/schema.sql
+++ WindAeroConstIEC/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model WindAeroConstIEC of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE WindAeroConstIEC_type AS ENUM ('WindAeroConstIEC');
-CREATE TABLE WindAeroConstIEC (WindGenTurbineType1IEC NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type WindAeroConstIEC_type);
+CREATE TABLE WindAeroConstIEC (WindGenTurbineType1IEC NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type WindAeroConstIEC_type);
