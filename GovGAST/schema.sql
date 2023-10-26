a6f65ecde34a4aa8049cb629649a82f48dce4e0c
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:05:10 +0200

    test of SQL export

--- GovGAST/schema.sql
+++ GovGAST/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model GovGAST of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE GovGAST_type AS ENUM ('GovGAST');
-CREATE TABLE GovGAST (address JSON, alternateName TEXT, areaServed TEXT, at NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, dturb NUMERIC, kt NUMERIC, mwbase NUMERIC, name TEXT, owner JSON, r NUMERIC, source TEXT, t1 NUMERIC, t2 NUMERIC, t3 NUMERIC, type GovGAST_type, vmax NUMERIC, vmin NUMERIC);
+CREATE TABLE GovGAST (address JSON, alternateName TEXT, areaServed TEXT, at NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, dturb NUMERIC, id TEXT PRIMARY KEY, kt NUMERIC, location JSON, mwbase NUMERIC, name TEXT, owner JSON, r NUMERIC, seeAlso JSON, source TEXT, t1 NUMERIC, t2 NUMERIC, t3 NUMERIC, type GovGAST_type, vmax NUMERIC, vmin NUMERIC);
