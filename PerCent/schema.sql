d2c92edd49143962a2c4cfdf18a3c0abaacc3fc9
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:08:13 +0200

    test of SQL export

--- PerCent/schema.sql
+++ PerCent/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model PerCent of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PerCent_type AS ENUM ('PerCent');
-CREATE TABLE PerCent (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multiplier NUMERIC, name TEXT, owner JSON, source TEXT, type PerCent_type, unit NUMERIC, value NUMERIC);
+CREATE TABLE PerCent (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, multiplier NUMERIC, name TEXT, owner JSON, seeAlso JSON, source TEXT, type PerCent_type, unit NUMERIC, value NUMERIC);
