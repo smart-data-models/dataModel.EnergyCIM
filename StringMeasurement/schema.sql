dd47ebb1ea9256f99c11c766adbcc438d1de2c7e
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:52:58 +0200

    test of SQL export

--- StringMeasurement/schema.sql
+++ StringMeasurement/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model StringMeasurement of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE StringMeasurement_type AS ENUM ('StringMeasurement');
-CREATE TABLE StringMeasurement (StringMeasurementValues NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type StringMeasurement_type);
+CREATE TABLE StringMeasurement (StringMeasurementValues NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type StringMeasurement_type);
