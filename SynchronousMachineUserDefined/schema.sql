34991fbb4814ad481bc63523d393bccc819d4d29
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:53:42 +0200

    test of SQL export

--- SynchronousMachineUserDefined/schema.sql
+++ SynchronousMachineUserDefined/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model SynchronousMachineUserDefined of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SynchronousMachineUserDefined_type AS ENUM ('SynchronousMachineUserDefined');
-CREATE TABLE SynchronousMachineUserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, proprietary NUMERIC, source TEXT, type SynchronousMachineUserDefined_type);
+CREATE TABLE SynchronousMachineUserDefined (ProprietaryParameterDynamics NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, proprietary NUMERIC, seeAlso JSON, source TEXT, type SynchronousMachineUserDefined_type);
