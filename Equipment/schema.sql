e844c1b2f66b550d9d14a0a44d25670f86e92903
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:02:13 +0200

    test of SQL export

--- Equipment/schema.sql
+++ Equipment/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Equipment of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Equipment_type AS ENUM ('Equipment');
-CREATE TABLE Equipment (EquipmentContainer NUMERIC, OperationalLimitSet NUMERIC, address JSON, aggregate NUMERIC, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type Equipment_type);
+CREATE TABLE Equipment (EquipmentContainer NUMERIC, OperationalLimitSet NUMERIC, address JSON, aggregate NUMERIC, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Equipment_type);
