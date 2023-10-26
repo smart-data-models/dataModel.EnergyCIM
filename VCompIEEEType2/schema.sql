a5e87e29a0d5a67642922f20c955f0cec5ce4fa7
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:54:58 +0200

    test of SQL export

--- VCompIEEEType2/schema.sql
+++ VCompIEEEType2/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model VCompIEEEType2 of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE VCompIEEEType2_type AS ENUM ('VCompIEEEType2');
-CREATE TABLE VCompIEEEType2 (GenICompensationForGenJ NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, tr NUMERIC, type VCompIEEEType2_type);
+CREATE TABLE VCompIEEEType2 (GenICompensationForGenJ NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, tr NUMERIC, type VCompIEEEType2_type);
