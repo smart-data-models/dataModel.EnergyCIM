55a09ae9f13ad9176e498c790a7f289506463e72
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:01:44 +0200

    test of SQL export

--- DiscExcContIEEEDEC3A/schema.sql
+++ DiscExcContIEEEDEC3A/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DiscExcContIEEEDEC3A of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DiscExcContIEEEDEC3A_type AS ENUM ('DiscExcContIEEEDEC3A');
-CREATE TABLE DiscExcContIEEEDEC3A (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, tdr NUMERIC, type DiscExcContIEEEDEC3A_type, vtmin NUMERIC);
+CREATE TABLE DiscExcContIEEEDEC3A (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, tdr NUMERIC, type DiscExcContIEEEDEC3A_type, vtmin NUMERIC);
