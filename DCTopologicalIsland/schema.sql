bc864956c8e5165bfc6e0329078a1ecc9c8aa940
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:01:18 +0200

    test of SQL export

--- DCTopologicalIsland/schema.sql
+++ DCTopologicalIsland/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DCTopologicalIsland of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DCTopologicalIsland_type AS ENUM ('DCTopologicalIsland');
-CREATE TABLE DCTopologicalIsland (DCTopologicalNodes NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type DCTopologicalIsland_type);
+CREATE TABLE DCTopologicalIsland (DCTopologicalNodes NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type DCTopologicalIsland_type);
