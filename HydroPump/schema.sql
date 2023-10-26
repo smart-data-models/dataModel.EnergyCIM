e8fe506550697d30973fb5a67b1995159d697a91
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:06:34 +0200

    test of SQL export

--- HydroPump/schema.sql
+++ HydroPump/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model HydroPump of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE HydroPump_type AS ENUM ('HydroPump');
-CREATE TABLE HydroPump (HydroPowerPlant NUMERIC, RotatingMachine NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type HydroPump_type);
+CREATE TABLE HydroPump (HydroPowerPlant NUMERIC, RotatingMachine NUMERIC, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type HydroPump_type);
