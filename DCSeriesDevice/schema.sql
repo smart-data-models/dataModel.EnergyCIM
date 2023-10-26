90a319849bb7b09d49554ac4b8b408861eae0bca
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 14:01:11 +0200

    test of SQL export

--- DCSeriesDevice/schema.sql
+++ DCSeriesDevice/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DCSeriesDevice of the subject dataModel.EnergyCIM for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DCSeriesDevice_type AS ENUM ('DCSeriesDevice');
-CREATE TABLE DCSeriesDevice (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, inductance NUMERIC, name TEXT, owner JSON, ratedUdc NUMERIC, resistance NUMERIC, source TEXT, type DCSeriesDevice_type);
+CREATE TABLE DCSeriesDevice (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, inductance NUMERIC, location JSON, name TEXT, owner JSON, ratedUdc NUMERIC, resistance NUMERIC, seeAlso JSON, source TEXT, type DCSeriesDevice_type);
