c950fe01dc352fc1f3b4b6e9de56d6e8db773475
Alberto Abella <alberto.abella@fiware.org>
2024-08-14 17:57:28 +0200

    Draft version of pydantic file v2

---
+++ ExcHU/code/code_for_using_pydantic.py
@@ -0,0 +1,214 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import AnyUrl, AwareDatetime, BaseModel, Field, RootModel, constr
+
+
+class Address(BaseModel):
+    addressCountry: Optional[str] = Field(
+        None, description='The country. For example, Spain'
+    )
+    addressLocality: Optional[str] = Field(
+        None,
+        description='The locality in which the street address is, and which is in the region',
+    )
+    addressRegion: Optional[str] = Field(
+        None,
+        description='The region in which the locality is, and which is in the country',
+    )
+    district: Optional[str] = Field(
+        None,
+        description='A district is a type of administrative division that, in some countries, is managed by the local government',
+    )
+    postOfficeBoxNumber: Optional[str] = Field(
+        None,
+        description='The post office box number for PO box addresses. For example, 03578',
+    )
+    postalCode: Optional[str] = Field(
+        None, description='The postal code. For example, 24004'
+    )
+    streetAddress: Optional[str] = Field(None, description='The street address')
+    streetNr: Optional[str] = Field(
+        None, description='Number identifying a specific property on a public street'
+    )
+
+
+class Type(Enum):
+    Point = 'Point'
+
+
+class Location(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[float] = Field(..., min_length=2)
+    type: Type
+
+
+class Coordinate(RootModel[List[float]]):
+    root: List[float]
+
+
+class Type1(Enum):
+    LineString = 'LineString'
+
+
+class Location1(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[Coordinate] = Field(..., min_length=2)
+    type: Type1
+
+
+class Type2(Enum):
+    Polygon = 'Polygon'
+
+
+class Location2(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type2
+
+
+class Type3(Enum):
+    MultiPoint = 'MultiPoint'
+
+
+class Location3(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[float]]
+    type: Type3
+
+
+class Type4(Enum):
+    MultiLineString = 'MultiLineString'
+
+
+class Location4(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type4
+
+
+class Type5(Enum):
+    MultiPolygon = 'MultiPolygon'
+
+
+class Location5(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[List[Coordinate]]]
+    type: Type5
+
+
+class Type6(Enum):
+    ExcHU = 'ExcHU'
+
+
+class ExcHU(BaseModel):
+    address: Optional[Address] = Field(None, description='The mailing address')
+    ae: Optional[float] = Field(
+        None,
+        description='Major loop PI tag gain factor (Ae).  Typical Value = 3. Default: 0.0',
+    )
+    ai: Optional[float] = Field(
+        None,
+        description='Minor loop PI tag gain factor (Ai).  Typical Value = 22. Default: 0.0',
+    )
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    areaServed: Optional[str] = Field(
+        None,
+        description='The geographic area where a service or offered item is provided',
+    )
+    atr: Optional[float] = Field(
+        None, description='AVR constant (Atr).  Typical Value = 2.19. Default: 0.0'
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    emax: Optional[float] = Field(
+        None,
+        description='Field voltage control signal upper limit on AVR base (Emax).  Typical Value = 0.996. Default: 0.0',
+    )
+    emin: Optional[float] = Field(
+        None,
+        description='Field voltage control signal lower limit on AVR base (Emin).  Typical Value = -0.866. Default: 0.0',
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    imax: Optional[float] = Field(
+        None,
+        description='Major loop PI tag output signal upper limit (Imax).  Typical Value = 2.19. Default: 0.0',
+    )
+    imin: Optional[float] = Field(
+        None,
+        description='Major loop PI tag output signal lower limit (Imin).  Typical Value = 0.1. Default: 0.0',
+    )
+    ke: Optional[float] = Field(
+        None,
+        description='Voltage base conversion constant (Ke).  Typical Value = 4.666. Default: 0.0',
+    )
+    ki: Optional[float] = Field(
+        None,
+        description='Current base conversion constant (Ki).  Typical Value = 0.21428. Default: 0.0',
+    )
+    location: Optional[
+        Union[Location, Location1, Location2, Location3, Location4, Location5]
+    ] = Field(
+        None,
+        description='Geojson reference to the item. It can be Point, LineString, Polygon, MultiPoint, MultiLineString or MultiPolygon',
+    )
+    name: Optional[str] = Field(None, description='The name of this item')
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='A sequence of characters giving the original source of the entity data as a URL. Recommended to be the fully qualified domain name of the source provider, or the URL to the source object',
+    )
+    te: Optional[float] = Field(
+        None,
+        description='Major loop PI tag integration time constant (Te).  Typical Value = 0.154. Default: 0',
+    )
+    ti: Optional[float] = Field(
+        None,
+        description='Minor loop PI control tag integration time constant (Ti).  Typical Value = 0.01333. Default: 0',
+    )
+    tr: Optional[float] = Field(
+        None,
+        description='Filter time constant (Tr). If a voltage compensator is used in conjunction with this excitation system model, Tr should be set to 0.  Typical Value = 0.01. Default: 0',
+    )
+    type: Optional[Type6] = Field(None, description='NGSI type. It has to be ExcHU')
