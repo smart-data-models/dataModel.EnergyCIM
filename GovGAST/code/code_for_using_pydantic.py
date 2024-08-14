efbd4c5e544821a13858da1d7fe00bb7676e49cc
Alberto Abella <alberto.abella@fiware.org>
2024-08-14 18:00:25 +0200

    Draft version of pydantic file v2

---
+++ GovGAST/code/code_for_using_pydantic.py
@@ -0,0 +1,205 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import AnyUrl, AwareDatetime, BaseModel, Field, RootModel, constr
+
+
+class Address(BaseModel):
+    addressCountry: Optional[str] = Field(
+        None, description='The country. For example, Spain'
+    )
+    addressLocality: Optional[str] = Field(
+        None,
+        description='The locality in which the street address is, and which is in the region',
+    )
+    addressRegion: Optional[str] = Field(
+        None,
+        description='The region in which the locality is, and which is in the country',
+    )
+    district: Optional[str] = Field(
+        None,
+        description='A district is a type of administrative division that, in some countries, is managed by the local government',
+    )
+    postOfficeBoxNumber: Optional[str] = Field(
+        None,
+        description='The post office box number for PO box addresses. For example, 03578',
+    )
+    postalCode: Optional[str] = Field(
+        None, description='The postal code. For example, 24004'
+    )
+    streetAddress: Optional[str] = Field(None, description='The street address')
+    streetNr: Optional[str] = Field(
+        None, description='Number identifying a specific property on a public street'
+    )
+
+
+class Type(Enum):
+    Point = 'Point'
+
+
+class Location(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[float] = Field(..., min_length=2)
+    type: Type
+
+
+class Coordinate(RootModel[List[float]]):
+    root: List[float]
+
+
+class Type1(Enum):
+    LineString = 'LineString'
+
+
+class Location1(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[Coordinate] = Field(..., min_length=2)
+    type: Type1
+
+
+class Type2(Enum):
+    Polygon = 'Polygon'
+
+
+class Location2(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type2
+
+
+class Type3(Enum):
+    MultiPoint = 'MultiPoint'
+
+
+class Location3(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[float]]
+    type: Type3
+
+
+class Type4(Enum):
+    MultiLineString = 'MultiLineString'
+
+
+class Location4(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type4
+
+
+class Type5(Enum):
+    MultiPolygon = 'MultiPolygon'
+
+
+class Location5(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[List[Coordinate]]]
+    type: Type5
+
+
+class Type6(Enum):
+    GovGAST = 'GovGAST'
+
+
+class GovGAST(BaseModel):
+    address: Optional[Address] = Field(None, description='The mailing address')
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    areaServed: Optional[str] = Field(
+        None,
+        description='The geographic area where a service or offered item is provided',
+    )
+    at: Optional[float] = Field(
+        None,
+        description='Ambient temperature load limit (Load Limit).  Typical Value = 1. Default: 0.0',
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    dturb: Optional[float] = Field(
+        None,
+        description='Turbine damping factor (Dturb).  Typical Value = 0.18. Default: 0.0',
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    kt: Optional[float] = Field(
+        None,
+        description='Temperature limiter gain (Kt).  Typical Value = 3. Default: 0.0',
+    )
+    location: Optional[
+        Union[Location, Location1, Location2, Location3, Location4, Location5]
+    ] = Field(
+        None,
+        description='Geojson reference to the item. It can be Point, LineString, Polygon, MultiPoint, MultiLineString or MultiPolygon',
+    )
+    mwbase: Optional[float] = Field(
+        None, description='Base for power values (MWbase) (> 0). Default: 0.0'
+    )
+    name: Optional[str] = Field(None, description='The name of this item')
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    r: Optional[float] = Field(
+        None, description='Permanent droop (R).  Typical Value = 0.04. Default: 0.0'
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='A sequence of characters giving the original source of the entity data as a URL. Recommended to be the fully qualified domain name of the source provider, or the URL to the source object',
+    )
+    t1: Optional[float] = Field(
+        None,
+        description='Governor mechanism time constant (T1).  T1 represents the natural valve positioning time constant of the governor for small disturbances, as seen when rate limiting is not in effect.  Typical Value = 0.5. Default: 0',
+    )
+    t2: Optional[float] = Field(
+        None,
+        description='Turbine power time constant (T2).  T2 represents delay due to internal energy storage of the gas turbine engine. T2 can be used to give a rough approximation to the delay associated with acceleration of the compressor spool of a multi-shaft engine, or with the compressibility of gas in the plenum of a the free power turbine of an aero-derivative unit, for example.  Typical Value = 0.5. Default: 0',
+    )
+    t3: Optional[float] = Field(
+        None,
+        description='Turbine exhaust temperature time constant (T3).  Typical Value = 3. Default: 0',
+    )
+    type: Optional[Type6] = Field(None, description='NGSI type. It has to be GovGAST')
+    vmax: Optional[float] = Field(
+        None,
+        description='Maximum turbine power, PU of MWbase (Vmax).  Typical Value = 1. Default: 0.0',
+    )
+    vmin: Optional[float] = Field(
+        None,
+        description='Minimum turbine power, PU of MWbase (Vmin).  Typical Value = 0. Default: 0.0',
+    )
