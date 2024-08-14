1dff36d31524aca2cbdaebd04187951660a9d33f
Alberto Abella <alberto.abella@fiware.org>
2024-08-14 18:08:25 +0200

    Draft version of pydantic file v2

---
+++ Quality61850/code/code_for_using_pydantic.py
@@ -0,0 +1,212 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import AnyUrl, AwareDatetime, BaseModel, Field, RootModel, constr
+
+
+class Address(BaseModel):
+    addressCountry: Optional[str] = Field(
+        None, description='The country. For example, Spain'
+    )
+    addressLocality: Optional[str] = Field(
+        None,
+        description='The locality in which the street address is, and which is in the region',
+    )
+    addressRegion: Optional[str] = Field(
+        None,
+        description='The region in which the locality is, and which is in the country',
+    )
+    district: Optional[str] = Field(
+        None,
+        description='A district is a type of administrative division that, in some countries, is managed by the local government',
+    )
+    postOfficeBoxNumber: Optional[str] = Field(
+        None,
+        description='The post office box number for PO box addresses. For example, 03578',
+    )
+    postalCode: Optional[str] = Field(
+        None, description='The postal code. For example, 24004'
+    )
+    streetAddress: Optional[str] = Field(None, description='The street address')
+    streetNr: Optional[str] = Field(
+        None, description='Number identifying a specific property on a public street'
+    )
+
+
+class Type(Enum):
+    Point = 'Point'
+
+
+class Location(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[float] = Field(..., min_length=2)
+    type: Type
+
+
+class Coordinate(RootModel[List[float]]):
+    root: List[float]
+
+
+class Type1(Enum):
+    LineString = 'LineString'
+
+
+class Location1(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[Coordinate] = Field(..., min_length=2)
+    type: Type1
+
+
+class Type2(Enum):
+    Polygon = 'Polygon'
+
+
+class Location2(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type2
+
+
+class Type3(Enum):
+    MultiPoint = 'MultiPoint'
+
+
+class Location3(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[float]]
+    type: Type3
+
+
+class Type4(Enum):
+    MultiLineString = 'MultiLineString'
+
+
+class Location4(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type4
+
+
+class Type5(Enum):
+    MultiPolygon = 'MultiPolygon'
+
+
+class Location5(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[List[Coordinate]]]
+    type: Type5
+
+
+class Type6(Enum):
+    Quality61850 = 'Quality61850'
+
+
+class Quality61850(BaseModel):
+    address: Optional[Address] = Field(None, description='The mailing address')
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    areaServed: Optional[str] = Field(
+        None,
+        description='The geographic area where a service or offered item is provided',
+    )
+    badReference: Optional[float] = Field(
+        None,
+        description='Measurement value may be incorrect due to a reference being out of calibration. Default: False',
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    estimatorReplaced: Optional[float] = Field(
+        None,
+        description='Value has been replaced by State Estimator. estimatorReplaced is not an IEC61850 quality bit but has been put in this class for convenience. Default: False',
+    )
+    failure: Optional[float] = Field(
+        None,
+        description='This identifier indicates that a supervision function has detected an internal or external failure, e.g. communication failure. Default: False',
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    location: Optional[
+        Union[Location, Location1, Location2, Location3, Location4, Location5]
+    ] = Field(
+        None,
+        description='Geojson reference to the item. It can be Point, LineString, Polygon, MultiPoint, MultiLineString or MultiPolygon',
+    )
+    name: Optional[str] = Field(None, description='The name of this item')
+    oldData: Optional[float] = Field(
+        None,
+        description='Measurement value is old and possibly invalid, as it has not been successfully updated during a specified time interval. Default: False',
+    )
+    operatorBlocked: Optional[float] = Field(
+        None,
+        description='Measurement value is blocked and hence unavailable for transmission. Default: False',
+    )
+    oscillatory: Optional[float] = Field(
+        None,
+        description='To prevent some overload of the communication it is sensible to detect and suppress oscillating (fast changing) binary inputs. If a signal changes in a defined time (tosc) twice in the same direction (from 0 to 1 or from 1 to 0) then oscillation is detected and the detail quality identifier `oscillatory` is set. If it is detected a configured numbers of transient changes could be passed by. In this time the validity status `questionable` is set. If after this defined numbers of changes the signal is still in the oscillating state the value shall be set either to the opposite state of the previous stable value or to a defined default value. In this case the validity status `questionable` is reset and `invalid` is set as long as the signal is oscillating. If it is configured such that no transient changes should be passed by then the validity status `invalid` is set immediately in addition to the detail quality identifier `oscillatory` (used for status information only). Default: False',
+    )
+    outOfRange: Optional[float] = Field(
+        None,
+        description='Measurement value is beyond a predefined range of value. Default: False',
+    )
+    overFlow: Optional[float] = Field(
+        None,
+        description='Measurement value is beyond the capability of being  represented properly. For example, a counter value overflows from maximum count back to a value of zero. Default: False',
+    )
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='Source gives information related to the origin of a value. The value may be acquired from the process, defaulted or substituted. Default: None',
+    )
+    suspect: Optional[float] = Field(
+        None,
+        description='A correlation function has detected that the value is not consitent with other values. Typically set by a network State Estimator. Default: False',
+    )
+    test: Optional[float] = Field(
+        None,
+        description='Measurement value is transmitted for test purposes. Default: False',
+    )
+    type: Optional[Type6] = Field(
+        None, description='NGSI type. It has to be Quality61850'
+    )
+    validity: Optional[float] = Field(
+        None, description='Validity of the measurement value. Default: None'
+    )
