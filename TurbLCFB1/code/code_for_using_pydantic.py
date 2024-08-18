0d845394e4483e9f45f959c45f44ad672054d500
Alberto Abella <alberto.abella@fiware.org>
2024-08-18 19:31:35 +0200

    Draft version of pydantic file v2

---
+++ TurbLCFB1/code/code_for_using_pydantic.py
@@ -0,0 +1,211 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import AnyUrl, AwareDatetime, BaseModel, Field, RootModel, constr
+
+
+class Address(BaseModel):
+    addressCountry: Optional[str] = Field(
+        None, description='The country. For example, Spain'
+    )
+    addressLocality: Optional[str] = Field(
+        None,
+        description='The locality in which the street address is, and which is in the region',
+    )
+    addressRegion: Optional[str] = Field(
+        None,
+        description='The region in which the locality is, and which is in the country',
+    )
+    district: Optional[str] = Field(
+        None,
+        description='A district is a type of administrative division that, in some countries, is managed by the local government',
+    )
+    postOfficeBoxNumber: Optional[str] = Field(
+        None,
+        description='The post office box number for PO box addresses. For example, 03578',
+    )
+    postalCode: Optional[str] = Field(
+        None, description='The postal code. For example, 24004'
+    )
+    streetAddress: Optional[str] = Field(None, description='The street address')
+    streetNr: Optional[str] = Field(
+        None, description='Number identifying a specific property on a public street'
+    )
+
+
+class Type(Enum):
+    Point = 'Point'
+
+
+class Location(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[float] = Field(..., min_length=2)
+    type: Type
+
+
+class Coordinate(RootModel[List[float]]):
+    root: List[float]
+
+
+class Type1(Enum):
+    LineString = 'LineString'
+
+
+class Location1(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[Coordinate] = Field(..., min_length=2)
+    type: Type1
+
+
+class Type2(Enum):
+    Polygon = 'Polygon'
+
+
+class Location2(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type2
+
+
+class Type3(Enum):
+    MultiPoint = 'MultiPoint'
+
+
+class Location3(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[float]]
+    type: Type3
+
+
+class Type4(Enum):
+    MultiLineString = 'MultiLineString'
+
+
+class Location4(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type4
+
+
+class Type5(Enum):
+    MultiPolygon = 'MultiPolygon'
+
+
+class Location5(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[List[Coordinate]]]
+    type: Type5
+
+
+class Type6(Enum):
+    TurbLCFB1 = 'TurbLCFB1'
+
+
+class TurbLCFB1(BaseModel):
+    address: Optional[Address] = Field(None, description='The mailing address')
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    areaServed: Optional[str] = Field(
+        None,
+        description='The geographic area where a service or offered item is provided',
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    db: Optional[float] = Field(
+        None, description='Controller dead band (db).  Typical Value = 0. Default: 0.0'
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    emax: Optional[float] = Field(
+        None,
+        description='Maximum control error (Emax) (note 4).  Typical Value = 0.02. Default: 0.0',
+    )
+    fb: Optional[float] = Field(
+        None, description='Frequency bias gain (Fb).  Typical Value = 0. Default: 0.0'
+    )
+    fbf: Optional[float] = Field(
+        None,
+        description='Frequency bias flag (Fbf). true = enable frequency bias false = disable frequency bias. Typical Value = false. Default: False',
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    irmax: Optional[float] = Field(
+        None,
+        description='Maximum turbine speed/load reference bias (Irmax) (note 3).  Typical Value = 0. Default: 0.0',
+    )
+    ki: Optional[float] = Field(
+        None, description='Integral gain (Ki).  Typical Value = 0. Default: 0.0'
+    )
+    kp: Optional[float] = Field(
+        None, description='Proportional gain (Kp).  Typical Value = 0. Default: 0.0'
+    )
+    location: Optional[
+        Union[Location, Location1, Location2, Location3, Location4, Location5]
+    ] = Field(
+        None,
+        description='Geojson reference to the item. It can be Point, LineString, Polygon, MultiPoint, MultiLineString or MultiPolygon',
+    )
+    mwbase: Optional[float] = Field(
+        None,
+        description='Base for power values (MWbase) (>0).  Unit = MW. Default: 0.0',
+    )
+    name: Optional[str] = Field(None, description='The name of this item')
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    pbf: Optional[float] = Field(
+        None,
+        description='Power controller flag (Pbf). true = enable load controller false = disable load controller. Typical Value = false. Default: False',
+    )
+    pmwset: Optional[float] = Field(
+        None,
+        description='Power controller setpoint (Pmwset) (note 1).  Unit = MW. Typical Value = 0. Default: 0.0',
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='A sequence of characters giving the original source of the entity data as a URL. Recommended to be the fully qualified domain name of the source provider, or the URL to the source object',
+    )
+    speedReferenceGovernor: Optional[float] = Field(
+        None,
+        description='Type of turbine governor reference (Type). true = speed reference governor false = load reference governor. Typical Value = true. Default: False',
+    )
+    tpelec: Optional[float] = Field(
+        None,
+        description='Power transducer time constant (Tpelec).  Typical Value = 0. Default: 0',
+    )
+    type: Optional[Type6] = Field(None, description='NGSI type. It has to be TurbLCFB1')
